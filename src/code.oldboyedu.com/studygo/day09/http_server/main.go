0169d00ae7ca1b26a61b411ed02939d9d851cc03
ahao214 <haochen214@163.com>
2020-11-22 20:28:35 +0800

    http

---
+++ src/code.oldboyedu.com/studygo/day09/http_server/main.go
@@ -0,0 +1,22 @@
+package main
+
+import(
+	"net/http"
+	"fmt"
+	"io/ioutil"
+)
+
+func f1(w http.ResponseWriter, r *http.Request){
+	//str:="hello lph!"
+	//w.Write([]byte(str))
+	b,err:=ioutil.ReadFile("./xx.txt")
+	if err!=nil{
+		w.Write([]byte(fmt.Sprintf("%v",err)))
+	}
+	w.Write(b)
+}
+
+func main(){
+	http.HandleFunc("/posts/Go/15_socket/",f1)
+	http.ListenAndServe("127.0.0.1:9090",nil)
+}
