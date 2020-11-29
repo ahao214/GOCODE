9e61d50cf535e8403174b1a9bb89232a7797ee26
ahao214 <haochen214@163.com>
2020-11-29 11:07:28 +0800

    context

---
+++ src/code.oldboyedu.com/studygo/day11/02context_demo/main.go
@@ -0,0 +1,36 @@
+package main
+
+import(
+	"fmt"
+	"time"
+	"sync"
+)
+
+//为什么需要context
+
+var wg sync.WaitGroup
+var exitChan=make(chan bool,1)
+
+func f(){
+	defer wg.Done()
+FORLOOP:
+	for{
+		fmt.Println("Tom")
+		time.Sleep(time.Millisecond*500)
+		select{
+		case <- exitChan:
+			break FORLOOP
+		default:
+		}
+	}
+	
+}
+
+func main(){
+	wg.Add(1)
+	go f()
+	time.Sleep(time.Second*5)
+	//如何通知子goroutine退出
+	exitChan<-true
+	wg.Wait()
+}
