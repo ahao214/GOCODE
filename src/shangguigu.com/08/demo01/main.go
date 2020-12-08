90f004d54ff5b6c4ab973bf7b9aadbbb03710944
ahao214 <haochen214@163.com>
2020-12-08 22:05:24 +0800

    可变参数

--- src/shangguigu.com/08/demo01/main.go
+++ src/shangguigu.com/08/demo01/main.go
@@ -11,9 +11,20 @@ func getSumAndSub(one int,two int)(sum int,sub int){
 	return
 }
 
+//GO支持可变参数
+func getSum(n int,args... int)int{
+	sum:=n
+	for i:=0;i<len(args);i++{
+		sum+=args[i]
+	}
+	return sum
+}
+
 func main(){
 
 	a,b:=getSumAndSub(20,10)
 	fmt.Printf("a:%v b:%v\n",a,b)
 
+	res:=getSum(1,23,3,4,-10,24)
+	fmt.Println(res)
 }
