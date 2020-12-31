53d9fdf42f5cd15f8745a33a27d9412ae4d083c6
ahao214 <haochen214@163.com>
2020-12-31 22:03:30 +0800

    工厂模式

--- src/shangguigu.com/Method/demo11/main/main.go
+++ src/shangguigu.com/Method/demo11/main/main.go
@@ -16,6 +16,6 @@ func main(){
 	//当结构体是小写字母开头，可以通过工厂模式来解决
 	var tea=model.NewTeacher("Alen",28)
 	fmt.Println(tea)
-	fmt.Println("name=",tea.Name,"age=",tea.Age)
+	fmt.Println("name=",tea.Name,"age=",tea.GetAge())
 
 }
--- src/shangguigu.com/Method/demo11/model/student.go
+++ src/shangguigu.com/Method/demo11/model/student.go
@@ -9,12 +9,18 @@ type Student struct{
 //通过工厂模式来解决
 type teacher struct{
 	Name string
-	Age int
+	age int
 }
 
 func NewTeacher(n string,a int)*teacher{
 	return &teacher{
 		Name:n,
-		Age:a,
+		age:a,
 	}
+}
+
+//如果teacher里面的Age首字母小写，则在其他包不可以直接访问
+//我们可以提供一个方法
+func(t *teacher) GetAge()int{
+	return t.age
 }
