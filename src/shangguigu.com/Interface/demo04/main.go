a2e4975585378cf19c92fc61af170212c88954a4
ahao214 <haochen214@163.com>
2021-01-02 21:28:12 +0800

    接口

---
+++ src/shangguigu.com/Interface/demo04/main.go
@@ -0,0 +1,48 @@
+package main
+
+import(
+	"fmt"
+)
+
+type Monkey struct{
+	Name string
+}
+
+
+//声明接口
+type BirdAble interface{
+	Flying()
+}
+
+type FishAble interface{
+	Swimming()
+}
+
+func(this *Monkey)climbing(){
+	fmt.Println(this.Name,"生来会爬树")
+}
+
+type LittleMonkey struct{
+	Monkey
+}
+
+//让LittleMonkey实现BirdAble
+func(this *LittleMonkey) Flying(){
+	fmt.Println(this.Name,"通过学习，学会飞翔...")
+}
+
+func(this *LittleMonkey)Swimming(){
+	fmt.Println(this.Name,"通过学习，学会游泳...")
+}
+
+func main(){
+
+	monkey:=LittleMonkey{
+		Monkey{
+			Name:"悟空",
+		},
+	}
+	monkey.climbing()
+	monkey.Flying()
+	monkey.Swimming()
+}
