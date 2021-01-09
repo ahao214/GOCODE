2b015550302d2b0e9a2b5bf717c0a9967d40b783
ahao214 <haochen214@163.com>
2021-01-09 19:40:36 +0800

    打开文件和关闭文件

--- src/shangguigu.com/File/demo01/main.go
+++ src/shangguigu.com/File/demo01/main.go
@@ -1,6 +1,22 @@
 package main
 
-func main(){
+import(
+	"fmt"
+	"os"
+)
 
+func main(){
+	//打开文件
+	file,err:=os.Open("f:/one.txt")
+	if err!=nil{
+		fmt.Println("open file err=",err)
+	}
+	fmt.Printf("file=%v",file)
 	
+	//关闭文件
+	err=file.Close()
+	if err!=nil{
+		fmt.Println("close file err=",err)
+	}
+
 }
