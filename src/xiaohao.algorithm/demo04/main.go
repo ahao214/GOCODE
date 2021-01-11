b3849ccb7b9bc78071dc391092128e7b1224aa5c
ahao214 <haochen214@163.com>
2021-01-11 17:36:10 +0800

    189.旋转数组

---
+++ src/xiaohao.algorithm/demo04/main.go
@@ -0,0 +1,22 @@
+package main
+
+
+//189.旋转数组
+func rotate(nums []int,k int){
+	reverse(nums)
+	reverse(nums[:k%len(nums)])
+	reverse(nums[k%len(nums):])
+}
+
+func reverse(arr[]int){
+	for i:=0;i<len(arr)/2;i++{
+		arr[i],arr[len(arr)-i-1]=arr[len(arr)-i-1],arr[i]
+	}
+}
+
+func main(){
+
+
+}
+
+
