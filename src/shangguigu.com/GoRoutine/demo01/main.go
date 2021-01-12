ebd46e52e526b6a4444b12f4619f7a2e40c09287
ahao214 <haochen214@163.com>
2021-01-12 14:20:25 +0800

    协程例子

--- src/shangguigu.com/GoRoutine/demo01/main.go
+++ src/shangguigu.com/GoRoutine/demo01/main.go
@@ -1,11 +1,23 @@
-package demo01
+package main
 
 import (
 	"fmt"
-
+	"strconv"
+	"time"
 )
 
+//编写一个函数，每隔1秒输出"hello world"
+func test(){
+	for i:=1;i<=10;i++{
+		fmt.Println("hello world"+strconv.Itoa(i))
+		time.Sleep(time.Second)
+	}
+}
+
 func main(){
-
-
+	go test()	//开启了一个协程
+	for i:=1;i<=10;i++{
+		fmt.Println("main hello golang"+strconv.Itoa(i))
+		time.Sleep(time.Second)
+	}
 }
