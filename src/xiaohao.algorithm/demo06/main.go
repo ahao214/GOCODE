ef04233702f30b2f50ca5325257668fb801a17ce
ahao214 <haochen214@163.com>
2021-01-12 19:07:17 +0800

    删除排序数组中的重复项

--- src/xiaohao.algorithm/demo06/main.go
+++ src/xiaohao.algorithm/demo06/main.go
@@ -1,7 +1,22 @@
 package main
 
+import(
+	"fmt"
+)
 
+//26:删除排序数组中的重复项
+func removeDuplicates(nums [] int)int{
+	for i:=0;i+1<len(nums);{
+		if nums[i]==nums[i+1]{
+			nums=append(nums[:i],nums[i+1:]...)
+		}else{
+			i++
+		}
+	}
+	return len(nums)
+}
 
 func main(){
-
+	nums:=[]int{0,1,1,2,3,3,4,4,5}
+	fmt.Println(removeDuplicates(nums))
 }
