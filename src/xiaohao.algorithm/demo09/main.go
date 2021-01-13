005cabcaf30c618fcc9394230c0ff33005294f78
ahao214 <haochen214@163.com>
2021-01-13 17:07:24 +0800

    爬楼梯

---
+++ src/xiaohao.algorithm/demo09/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+//70.爬楼梯
+func climbStairs(n int) int {
+	if n == 1 {
+		return 1
+	}
+	dp := make([]int, n+1)
+	dp[1] = 1
+	dp[2] = 2
+	for i := 3; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	return dp[n]
+
+}
+
+func main() {
+	n := 12
+	fmt.Println(climbStairs(n))
+
+}
