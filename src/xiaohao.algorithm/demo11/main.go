6210a15d7577274a74c44f1e827648eb5b4a94cb
ahao214 <haochen214@163.com>
2021-01-13 18:13:07 +0800

    最长上升子序列

---
+++ src/xiaohao.algorithm/demo11/main.go
@@ -0,0 +1,32 @@
+package main
+
+//300.最长上升子序列
+//给定一个无序的整数数组，找到其中最长上升子序列的长度
+func lengthOfLIS(nums []int) int {
+	if len(nums) < 1 {
+		return 0
+	}
+	dp := make([]int, len(nums))
+	result := 1
+	for i := 0; i < len(nums); i++ {
+		dp[i] = 1
+		for j := 0; j < i; j++ {
+			if nums[j] < nums[i] {
+				dp[i] = max(dp[j]+1, dp[i])
+			}
+		}
+		result = max(result, dp[i])
+	}
+	return result
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+
+}
