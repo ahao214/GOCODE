246854caf7505d54c661c1ad77f9627985abc565
ahao214 <haochen214@163.com>
2021-01-13 14:27:53 +0800

    两数之和

--- src/xiaohao.algorithm/demo08/main.go
+++ src/xiaohao.algorithm/demo08/main.go
@@ -16,8 +16,23 @@ func twoSum(nums []int, target int) []int {
 	return []int{}
 }
 
+//方法2
+func twoSum2(nums []int, target int) []int {
+	result := []int{}
+	m := make(map[int]int)
+	for i, k := range nums {
+		if value, exist := m[target-k]; exist {
+			result = append(result, value)
+			result = append(result, i)
+		}
+		m[k] = i
+	}
+	return result
+}
+
 func main() {
 	nums := []int{2, 3, 7, 9, 1, 4}
 	target := 9
 	fmt.Println(twoSum(nums, target))
+	fmt.Println(twoSum2(nums, target))
 }
