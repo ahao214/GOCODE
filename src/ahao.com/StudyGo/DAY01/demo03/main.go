4ea2b8ada7600039b7b1ae19a190597c944f93f7
ahao214 <haochen214@163.com>
2021-01-14 15:11:06 +0800

    map

--- src/ahao.com/StudyGo/DAY01/demo03/main.go
+++ src/ahao.com/StudyGo/DAY01/demo03/main.go
@@ -49,4 +49,38 @@ func main() {
 	for k, v := range area1 {
 		fmt.Println(k, v)
 	}
+
+	//声明map
+	var m1 map[string]string
+	//使用make函数创建一个非nil的map，nil map不能赋值
+	m1 = make(map[string]string)
+	//给声明的map赋值
+	m1["a"] = "aa"
+	m1["b"] = "bb"
+
+	//直接创建
+	m2 := make(map[string]string)
+	m2["c"] = "cc"
+
+	//初始化+赋值一体化
+	m3 := map[string]string{
+		"d": "dd",
+		"e": "ee",
+	}
+
+	//查找键值是否存在
+	if v, ok := m1["a"]; ok {
+		fmt.Println(v)
+	} else {
+		fmt.Println("key not found")
+	}
+
+	//遍历map
+	for k, v := range m3 {
+		fmt.Println(k, v)
+	}
+
+	for k, v := range m1 {
+		fmt.Println(k, v)
+	}
 }
