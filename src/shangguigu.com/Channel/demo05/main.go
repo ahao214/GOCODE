b525362446373502e1b56d79e2b9733fe2fc0426
ahao214 <haochen214@163.com>
2021-01-16 16:56:49 +0800

    管道只读和只写

---
+++ src/shangguigu.com/Channel/demo05/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	//声明管道为只写
+	var writeChan chan<- int
+	writeChan = make(chan int, 3)
+	writeChan <- 100
+
+	fmt.Println(writeChan)
+
+	//声明管道为只读
+	var readChan <-chan int
+	num := <-readChan
+	fmt.Println(num)
+}
