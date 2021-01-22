49ce6b1f6d53a7f61788a86138de9e65e41d778e
ahao214 <haochen214@163.com>
2021-01-22 10:15:48 +0800

    TCP编程

---
+++ src/ahao.com/StudyGo/DAY07/TCP/Server/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+// TCP Server端
+
+// 处理函数
+func process(conn net.Conn) {
+	//关闭连接
+	defer conn.Close()
+	for {
+		reader := bufio.NewReader(conn)
+		var buf [128]byte
+		n, err := reader.Read(buf[:])
+		if err != nil {
+			fmt.Println("read from client failed,err=", err)
+			break
+		}
+		recvStr := string(buf[:n])
+		fmt.Println("收到client端发来的数据：", recvStr)
+		conn.Write([]byte(recvStr)) //发送数据
+	}
+}
+
+//使用Go语言的net包实现的TCP服务端代码如下
+func main() {
+	listen, err := net.Listen("tcp", "127.0.0.1:20000")
+	if err != nil {
+		fmt.Println("listen failed,err:", err)
+		return
+	}
+
+	for {
+		conn, err := listen.Accept() //建立连接
+		if err != nil {
+			fmt.Println("accept failed,err:", err)
+			continue
+		}
+		go process(conn) //启动一个goroutine处理连接
+	}
+
+}
