5d978d25dfb97233c598b82bbdc1d11637058526
ahao214 <haochen214@163.com>
2021-01-25 15:48:00 +0800

    goroutine

---
+++ src/ahao.com/StudyGo/DAY08/Goroutine/demo01/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func hello() {
+	fmt.Println("Hello Goroutine!")
+}
+
+//使用time.Sleep来实现goroutine
+func main() {
+	go hello() // 启动另外一个goroutine去执行hello函数
+	fmt.Println("main goroutine done!")
+	time.Sleep(time.Second)
+}
