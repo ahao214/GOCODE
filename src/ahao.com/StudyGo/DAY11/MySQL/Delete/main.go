187bfcb7a83468e815cfa89ea2e30d04fd7f8fce
ahao214 <haochen214@163.com>
2021-02-07 11:04:51 +0800

    MySQL Delete操作

---
+++ src/ahao.com/StudyGo/DAY11/MySQL/Delete/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jmoiron/sqlx"
+)
+
+type Person struct {
+	UserId   int    `db:user_id`
+	UserName string `db:username`
+	Sex      string `db:sex`
+	Email    string `db:email`
+}
+
+type Place struct {
+	Country string `db:country`
+	City    string `db:city`
+	TelCode int    `db:telcode`
+}
+
+var Db *sqlx.DB
+
+func init() {
+	database, err := sqlx.Open("mysql", "root:root@tcp(127.0.0.1:3306)/text")
+	if err != nil {
+		fmt.Println("open mysql failed,", err)
+		return
+	}
+	Db = database
+	defer Db.Close()
+}
+
+func main() {
+	res, err := Db.Exec("delete from person where user_id=?", 1)
+	if err != nil {
+		fmt.Println("exec failed,", err)
+		return
+	}
+	row, err := res.RowsAffected()
+	if err != nil {
+		fmt.Println("rows failed,", err)
+	}
+	fmt.Println("delete success:", row)
+
+}
