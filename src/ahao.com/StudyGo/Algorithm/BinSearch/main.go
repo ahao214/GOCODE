1e717de6d9473f360569a0362fb6919d32ea5406
ahao214 <haochen214@163.com>
2021-02-10 10:43:48 +0800

    二分查找方法

---
+++ src/ahao.com/StudyGo/Algorithm/BinSearch/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+func binSearch(arr []int, finddata int) int {
+	low := 0
+	high := len(arr) - 1
+	for low <= high {
+		mid := (low + high) / 2
+		fmt.Println(mid)
+		if arr[mid] > finddata {
+			high = mid - 1
+		} else if arr[mid] < finddata {
+			low = mid + 1
+		} else {
+			return mid
+		}
+	}
+	return -1
+}
+
+//二分查找方法
+func main() {
+	arr := make([]int, 1024*1024, 1024*1024)
+	for i := 0; i < 1024*1024; i++ {
+		arr[i] = i + 1
+	}
+	id := binSearch(arr, 1024)
+	if id != -1 {
+		fmt.Println(id, arr[id])
+	} else {
+		fmt.Println("没有找到数据")
+	}
+}
