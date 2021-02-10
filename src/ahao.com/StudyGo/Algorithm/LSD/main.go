b30d9696a6a420fa2aa6493bac64170d89be6774
ahao214 <haochen214@163.com>
2021-02-10 17:41:32 +0800

    LSD

---
+++ src/ahao.com/StudyGo/Algorithm/LSD/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+//基数排序算法
+func main() {
+	var arr [3][]int
+	myarr := []int{1, 2, 3, 1, 1, 2, 2, 2, 2, 2, 3}
+	for i := 0; i < len(myarr); i++ {
+		arr[myarr[i]-1] = append(arr[myarr[i]-1], myarr[i])
+	}
+	fmt.Println(arr)
+}
