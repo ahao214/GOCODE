da05feafae3579cfc8087c314a0cdc6f4b742a60
ahao214 <haochen214@163.com>
2021-03-13 21:47:55 +0800

    文件操作

---
+++ src/ahao.com/chapter13/demo01/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	os.Mkdir("goDir", 0777)
+	os.MkdirAll("goDir/test1/text2", 0777)
+	err := os.Remove("goDir")
+	if err != nil {
+		fmt.Println(err)
+	}
+	os.RemoveAll("goDir")
+
+}
