217f37879f6fcaac8e40fa9cda9947b53e731e46
ahao214 <haochen214@163.com>
2021-03-15 11:27:55 +0800

    流量控

--- src/ahao.com/StudyGo/DAY15/Videos/limiter.go
+++ src/ahao.com/StudyGo/DAY15/Videos/limiter.go
@@ -1,6 +1,36 @@
 package main
 
-//流量控
-func main() {
+import (
+	"log"
+)
 
+//ConnLimiter 定义一个结构体
+type ConnLimiter struct {
+	concurrentConn int
+	bucket         chan int
+}
+
+//NewConnLimiter ...
+func NewConnLimiter(cc int) *ConnLimiter {
+	return &ConnLimiter{
+		concurrentConn: cc,
+		bucket:         make(chan int, cc),
+	}
+}
+
+//GetConn 获取通道里面的值
+func (cl *ConnLimiter) GetConn() bool {
+	if len(cl.bucket) >= cl.concurrentConn {
+		log.Printf("Reached the rate limitation.")
+		return false
+	}
+
+	cl.bucket <- 1
+	return true
+}
+
+//ReleaseConn 释放通道里面的值
+func (cl *ConnLimiter) ReleaseConn() {
+	c := <-cl.bucket
+	log.Printf("New connction coming: %d", c)
 }
--- src/ahao.com/StudyGo/DAY15/Videos/main.go
+++ src/ahao.com/StudyGo/DAY15/Videos/main.go
@@ -1,6 +1,55 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"text/template"
+	"time"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+type middleWareHandler struct {
+	r *httprouter.Router
+	l *ConnLimiter
+}
+
+//NewMiddleWareHandler ...
+func NewMiddleWareHandler(r *httprouter.Router, cc int) http.Handler {
+	m := middleWareHandler{}
+	m.r = r
+	m.l = NewConnLimiter(cc)
+	return m
+}
+
+func (m middleWareHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if !m.l.GetConn() {
+		defer func() { recover() }()
+		log.Panicln("Too many requests")
+		return
+	}
+	m.r.ServeHTTP(w, r)
+	defer m.l.ReleaseConn()
+}
+
+//RegisterHandlers ...
+func RegisterHandlers() *httprouter.Router {
+	router := httprouter.New()
+	router.GET("/ce", ce)
+	return router
+}
+
+func ce(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	//为了演示效果这块设置了等待
+	time.Sleep(time.Second * 100)
+	t, _ := template.ParseFiles("./videos/ce.html")
+	t.Execute(w, nil)
+}
+
 //流量控
 func main() {
-
+	r := RegisterHandlers()
+	//里面的参数2为设置的最大流量
+	mh := NewMiddleWareHandler(r, 2)
+	http.ListenAndServe(":9000", mh)
 }
