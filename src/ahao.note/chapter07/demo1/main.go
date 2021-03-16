5df3fd2575e466b04d6273746f275cf897a9ead8
ahao214 <haochen214@163.com>
2021-03-16 21:06:42 +0800

    接口

---
+++ src/ahao.note/chapter07/demo1/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+type TestError struct {
+}
+
+func (*TestError) Error() string {
+	return "error"
+}
+
+func test(x int) (int, error) {
+	var err *TestError
+	if x < 0 {
+		err = new(TestError)
+		x = 0
+	} else {
+		x += 100
+	}
+	return x, err
+}
+
+func main() {
+	x, err := test(100)
+	if err != nil {
+		log.Fatalln("err!=nil")
+	}
+	fmt.Println(x)
+
+}
