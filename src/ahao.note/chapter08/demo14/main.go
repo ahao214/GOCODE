c713a185b8030c94bce981d54406ca605626e30d
ahao214 <haochen214@163.com>
2021-03-17 14:45:46 +0800

    同步

---
+++ src/ahao.note/chapter08/demo14/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type cache struct {
+	sync.Mutex
+	data []int
+}
+
+func (c *cache) count() int {
+	c.Lock()
+	n := len(c.data)
+	c.Unlock()
+	return n
+}
+
+func (c *cache) get() int {
+	c.Lock()
+	defer c.Unlock()
+
+	var d int
+	if n := c.count(); n > 0 {
+		d = c.data[0]
+		c.data = c.data[1:]
+	}
+	return d
+}
+
+//同步
+func main() {
+	c := cache{
+		data: []int{1, 2, 3, 4},
+	}
+	fmt.Println(c.get())
+}
