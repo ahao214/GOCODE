6ccd270973291f5eb0929f351b25cbd60752ce53
ahao214 <haochen214@163.com>
2021-03-17 09:55:02 +0800

    并发

---
+++ src/ahao.note/chapter08/demo1/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var c int
+
+func counter() int {
+	c++
+	return c
+}
+
+func main() {
+	a := 100
+	go func(x, y int) {
+		time.Sleep(time.Second)
+		fmt.Println("go:", x, y)
+	}(a, counter())
+	a += 10
+	fmt.Println("main:", a, counter())
+	//等待goroutine结束
+	time.Sleep(time.Second * 3)
+}
