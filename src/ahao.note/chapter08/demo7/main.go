86311ce937ebd4225e8828fca9fab21c3de87272
ahao214 <haochen214@163.com>
2021-03-17 10:41:22 +0800

    Gosched

---
+++ src/ahao.note/chapter08/demo7/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	runtime.GOMAXPROCS(1)
+	exit := make(chan struct{})
+	go func() {
+		defer close(exit)
+
+		go func() {
+			fmt.Println("b")
+		}()
+		for i := 0; i < 4; i++ {
+			fmt.Println("a", i)
+			if i == 1 {
+				runtime.Gosched()
+			}
+		}
+	}()
+	<-exit
+}
