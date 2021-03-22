53797bab5667b0be3996f6a8a7e36821f8ad573a
ahao214 <haochen214@163.com>
2021-03-22 12:52:44 +0800

    panic

---
+++ src/ahao.note/chapter02/demo10/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func do() {
+	//这里并不能捕获do函数中panic
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	go da()
+	go db()
+	time.Sleep(3 * time.Second)
+}
+
+func da() {
+	panic("panic da")
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+}
+
+func db() {
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+}
