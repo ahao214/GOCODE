19edb92bac226db70559d0d92636790527a11974
ahao214 <haochen214@163.com>
2021-03-25 15:39:51 +0800

    查找重复的行

--- src/ahao.Bible/chapter01/demo3/main.go
+++ src/ahao.Bible/chapter01/demo3/main.go
@@ -1,1 +1,22 @@
-package demo3
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+//查找重复的行
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+
+}
