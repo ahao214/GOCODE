fc8ac87c41766a3628d8139e8444261a23966121
ahao214 <haochen214@163.com>
2021-03-29 16:33:05 +0800

    类别数据库操作

---
+++ src/Ahao.Blogger/dao/db/category.go
@@ -0,0 +1,42 @@
+package db
+
+import (
+	"Ahao.Blogger/model"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/jmoiron/sqlx"
+)
+
+func InsertCategory(category *model.Category) (categoryId int64, err error) {
+	sqlstr := "insert into category(category_name,category_no)values(?,?)"
+	result, err := DB.Exec(sqlstr, category.CategoryName, category.CategoryNo)
+	if err != nil {
+		return
+	}
+	categoryId, err = result.LastInsertId()
+	return
+}
+
+func GetCategoryById(id int64) (category *model.Category, err error) {
+	category = &model.Category{}
+	sqlstr := "select id,category_name,category_no from cagegory where id=?"
+	err = DB.Get(category, sqlstr, id)
+	return
+}
+
+func GetCategoryList(categoryIds []int64) (categoryList []*model.Category, err error) {
+	sqlstr, args, err := sqlx.In("select id,category_name,"+
+		"category_no from category where id(?)", categoryIds)
+	if err != nil {
+		return
+	}
+	err = DB.Select(&categoryList, sqlstr, args...)
+	return
+
+}
+
+func GetAllCategoryList() (categoryList []*model.Category, err error) {
+	sqlstr := "select id,category_name,category_no from category order by category_no asc"
+	err = DB.Select(&categoryList, sqlstr)
+	return
+}
--- src/Ahao.Blogger/service/article.go
+++ src/Ahao.Blogger/service/article.go
@@ -1,1 +1,43 @@
 package service
+
+import (
+	"Ahao.Blogger/dao/db"
+	"Ahao.Blogger/model"
+	"fmt"
+	"math"
+)
+
+func GetArticleRecordList(pageNum, pageSize int) (articleRecordList []*model.ArticleRecord, err error) {
+	articleInfoList, err := db.GetArticleList(pageNum, pageSize)
+	if err != nil {
+		fmt.Printf("get aritcle list failed,err:%v\n", err)
+		return
+	}
+	if len(articleInfoList) == 0 {
+		return
+	}
+	categoryIds := getCategoryIds(articleInfoList)
+	categoryList, err := db.GetCategoryList(categoryIds)
+	if err != nil {
+		fmt.Printf("get category list failed,err:%v\n", err)
+		return
+	}
+	for _, article := range articleInfoList {
+		articleRecord := &model.ArticleRecord{
+			ArticleInfo: *article,
+		}
+		categoryId := article.CategoryId
+		for _, category := range categoryList {
+			if categoryId == category.CategoryId {
+				articleRecord.Category = *category
+				break
+			}
+		}
+		articleRecordList = append(articleRecordList, articleRecord)
+	}
+	return
+}
+
+func getCategoryIds(articleInfoList []*model.ArticleInfo) (ids []int64) {
+
+}
