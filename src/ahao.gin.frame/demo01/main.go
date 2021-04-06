4e45d6b65eafef6bafb82362aff7d6e39ef986c6
ahao214 <haochen214@163.com>
2021-04-06 12:38:26 +0800

    gin中get/post/put/delete接口

--- src/ahao.gin.frame/demo01/main.go
+++ src/ahao.gin.frame/demo01/main.go
@@ -1,1 +1,41 @@
 package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+	r.GET("/path/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		//user为空，获取默认值aho
+		user := c.DefaultQuery("user", "aho")
+		pwd := c.Query("pwd")
+		c.JSON(200, gin.H{
+			"id":   id,
+			"user": user,
+			"pwd":  pwd,
+		})
+	})
+	r.POST("/path", func(c *gin.Context) {
+		user := c.DefaultPostForm("user", "aho")
+		pwd := c.PostForm("pwd")
+		c.JSON(200, gin.H{
+			"user": user,
+			"pwd":  pwd,
+		})
+	})
+	r.DELETE("/path/:id", func(c *gin.Context) {
+		id := c.Query("id")
+		c.JSON(200, gin.H{
+			"id": id,
+		})
+	})
+	r.PUT("/path", func(c *gin.Context) {
+		user := c.DefaultPostForm("user", "aho")
+		pwd := c.PostForm("pwd")
+		c.JSON(200, gin.H{
+			"user": user,
+			"pwd":  pwd,
+		})
+	})
+	r.Run(":8000")
+}
