7a35233e634668ad4e3a4d8ed436f8f22f1ca50d
ahao214 <haochen214@163.com>
2021-05-12 22:02:37 +0800

    找出数组中唯一的重复元素-异或方法

--- src/ahao.algorithm/chapter04/demo01/main.go
+++ src/ahao.algorithm/chapter04/demo01/main.go
@@ -19,9 +19,27 @@ func FindDupByHash(arr []int) int {
 	return -1
 }
 
+//异或方法
+func FindDupByXOR(arr []int) int {
+	if arr == nil {
+		return -1
+	}
+	result := 0
+	len := len(arr)
+	for _, v := range arr {
+		result ^= v
+	}
+	for i := 1; i < len; i++ {
+		result ^= i
+	}
+	return result
+}
+
 func main() {
 	arr := []int{1, 3, 4, 2, 5, 3}
 	fmt.Println("Hash方法")
 	fmt.Println(FindDupByHash(arr))
 
+	fmt.Println("异或方法")
+	fmt.Println(FindDupByXOR(arr))
 }
