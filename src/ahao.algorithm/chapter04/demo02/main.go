333b5168a40b44a6a8f5506e1e037222c3717456
ahao214 <haochen214@163.com>
2021-05-14 18:14:31 +0800

    查询数组中元素的最大值和最小值-变形的分冶方法

--- src/ahao.algorithm/chapter04/demo02/main.go
+++ src/ahao.algorithm/chapter04/demo02/main.go
@@ -49,6 +49,51 @@ func GetMaxAndMin(arr []int) (max, min int) {
 	return
 }
 
+//变形的分冶方法
+func GetMaxAndMinRe(arr []int, l, r int) (max, min int) {
+	if arr == nil {
+		return
+	}
+	//求中点
+	m := (l + r) / 2
+	//l与r之间只有一个元素
+	if l == r {
+		max = arr[l]
+		min = arr[l]
+		return
+	}
+	//l与r之间只有两个元素
+	if l+1 == r {
+		if arr[l] >= arr[r] {
+			max = arr[l]
+			min = arr[r]
+		} else {
+			max = arr[r]
+			min = arr[l]
+		}
+		return
+	}
+
+	//递归计算左半部分
+	lmax, lmin := GetMaxAndMinRe(arr, l, m)
+
+	//递归计算有半部分
+	rmax, rmin := GetMaxAndMinRe(arr, m+1, r)
+	//总的最大值
+	if lmax > rmax {
+		max = lmax
+	} else {
+		max = rmax
+	}
+	if lmin < rmin {
+		min = lmin
+	} else {
+		min = rmin
+	}
+
+	return
+}
+
 func main() {
 	arr := []int{7, 3, 19, 40, 4, 7, 1}
 	max, min := GetMaxAndMin(arr)
@@ -56,5 +101,9 @@ func main() {
 	fmt.Println("最大值:", max)
 	fmt.Println("最小值:", min)
 
-	fmt.Println("")
+	fmt.Println("扩展分冶方法")
+	rmax, rmin := GetMaxAndMinRe(arr, 0, len(arr)-1)
+	fmt.Println("最大值:", rmax)
+	fmt.Println("最小值:", rmin)
+
 }
