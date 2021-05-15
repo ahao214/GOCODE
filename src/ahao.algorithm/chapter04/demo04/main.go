3acdeae8582a098e39bfd698c9a58e977489ffbf
ahao214 <haochen214@163.com>
2021-05-15 18:11:49 +0800

    找出数组中丢失的数-异或方法

--- src/ahao.algorithm/chapter04/demo04/main.go
+++ src/ahao.algorithm/chapter04/demo04/main.go
@@ -21,8 +21,27 @@ func getNum(arr []int) int {
 	return sumb - suma
 }
 
+//异或方法
+func getNumXor(arr []int) int {
+	if arr == nil || len(arr) == 0 {
+		return -1
+	}
+	a := arr[0]
+	b := 1
+	len := len(arr)
+	for i := 1; i < len; i++ {
+		a ^= arr[i]
+	}
+	for i := 2; i <= len+1; i++ {
+		b ^= i
+	}
+	return a ^ b
+}
+
 func main() {
 	fmt.Println("累加求和：")
 	arr := []int{1, 4, 3, 2, 7, 5}
 	fmt.Println(getNum(arr))
+	fmt.Println("异或方法")
+	fmt.Println(getNumXor(arr))
 }
