35acbdee000cfd9b0f45bd1bc5a63c136e6aae8b
ahao214 <haochen214@163.com>
2021-05-16 16:32:42 +0800

    求解最小三元组距离-最小距离法

--- src/ahao.algorithm/chapter04/demo08/main.go
+++ src/ahao.algorithm/chapter04/demo08/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	. "github.com/isdamir/gotype"
+	"math"
 )
 
 //求解最小三元组距离
@@ -26,10 +27,53 @@ func minDistance(a, b, c []int) int {
 	return minDist
 }
 
+//最小距离法
+func minDistanceDyn(a, b, c []int) int {
+	aLen := len(a)
+	bLen := len(b)
+	cLen := len(c)
+	minDist := math.MaxInt64
+
+	//数组a的下标
+	i := 0
+	//数组b的下标
+	j := 0
+	//数组c的下标
+	k := 0
+
+	for true {
+		curDist := Max3(Abs(a[i]-b[j]), Abs(a[i]-c[k]), Abs(b[j]-c[k]))
+		if curDist < minDist {
+			minDist = curDist
+		}
+		//找出当前遍历到三个数组中的最小值
+		min := Min3(a[i], b[j], c[k])
+		if min == a[i] {
+			i++
+			if i >= aLen {
+				break
+			}
+		} else if min == b[j] {
+			j++
+			if j >= bLen {
+				break
+			}
+		} else {
+			k++
+			if k >= cLen {
+				break
+			}
+		}
+	}
+	return minDist
+}
+
 func main() {
 	a := []int{3, 4, 5, 7, 15}
 	b := []int{10, 12, 14, 16, 17}
 	c := []int{20, 21, 23, 24, 37, 30}
 	fmt.Println("蛮力法")
 	fmt.Println("最小距离为：", minDistance(a, b, c))
+	fmt.Println("最小距离法")
+	fmt.Println("最小距离为：", minDistanceDyn(a, b, c))
 }
