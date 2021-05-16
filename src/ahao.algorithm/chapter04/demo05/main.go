1581a11bc3964bf63011e771dc2c57acd36a9015
ahao214 <haochen214@163.com>
2021-05-16 15:18:32 +0800

    找出数组中出现奇数次的数

--- src/ahao.algorithm/chapter04/demo05/main.go
+++ src/ahao.algorithm/chapter04/demo05/main.go
@@ -1,7 +1,36 @@
 package main
 
+import "fmt"
+
 //找出数组中出现奇数次的数
+func getNum(arr []int) {
+	if arr == nil || len(arr) < 1 {
+		return
+	}
+	data := map[int]int{}
+	for _, v := range arr {
+		if vv, ok := data[v]; ok {
+			if vv == 1 {
+				data[v] = 0
+			} else {
+				data[v] = 1
+			}
+		} else {
+			//map中没有这个数字，说明第一次出现，value赋值为1
+			data[v] = 1
+		}
+	}
+
+	for _, v := range arr { //使用数组来遍历，防止出现map的key随机分布
+		if data[v] == 1 {
+			fmt.Println(v)
+		}
+	}
+}
 
 func main() {
+	fmt.Println("Hash 法")
+	arr := []int{3, 5, 6, 6, 5, 7, 2, 2}
+	getNum(arr)
 
 }
