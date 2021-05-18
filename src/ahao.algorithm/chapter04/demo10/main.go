25d363a49f45cfc57261d90765a0a2e16570e046
ahao214 <haochen214@163.com>
2021-05-18 16:31:15 +0800

    重复利用已经计算的子数组和

--- src/ahao.algorithm/chapter04/demo10/main.go
+++ src/ahao.algorithm/chapter04/demo10/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 //求数组中连续最大和
 
@@ -29,8 +32,29 @@ func maxSubArray(arr []int) int {
 	return MaxSum
 }
 
+//重复利用已经计算的子数组和
+func maxSubArraySum(arr []int) int {
+	if arr == nil || len(arr) < 1 {
+		return -1
+	}
+	len := len(arr)
+	maxSum := math.MinInt64
+	for i := 0; i < len; i++ {
+		sum := 0
+		for j := i; j < len; j++ {
+			sum += arr[j]
+			if sum > maxSum {
+				maxSum = sum
+			}
+		}
+	}
+	return maxSum
+}
+
 func main() {
 	arr := []int{1, -2, 4, 8, -4, 7, -1, -5}
 	fmt.Println("蛮力法")
 	fmt.Println("连续最大和为：", maxSubArray(arr))
+	fmt.Println("重复利用已经计算的子数组和")
+	fmt.Println("连续最大和为：", maxSubArraySum(arr))
 }
