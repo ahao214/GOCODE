2102efa02b9451ce3b108501ad8fe992072feb2b
ahao214 <haochen214@163.com>
2021-05-20 19:09:58 +0800

    求集合的所有子集-迭代法

--- src/ahao.algorithm/chapter04/demo14/main.go
+++ src/ahao.algorithm/chapter04/demo14/main.go
@@ -23,10 +23,32 @@ func getAllSybset(arr []rune, mask []int, c int) {
 	}
 }
 
+//迭代法
+func getAllSybsetSub(str string) (arr []string) {
+	arr = make([]string, 0)
+	if str == "" {
+		return arr
+	}
+	arr = append(arr, string(str[0]))
+	for i := 1; i < len(str); i++ {
+		ll := len(arr)
+		for j := 0; j < ll; j++ {
+			arr = append(arr, arr[j]+string(str[i]))
+		}
+		arr = append(arr, string(str[i:i+1]))
+	}
+	return arr
+}
+
 func main() {
 	arr := []rune{'a', 'b', 'c'}
 	mask := []int{0, 0, 0}
 	fmt.Println("位图法")
 	getAllSybset(arr, mask, 0)
 
+	fmt.Println("迭代法")
+	result := getAllSybsetSub("abc")
+	for _, v := range result {
+		fmt.Println(v)
+	}
 }
