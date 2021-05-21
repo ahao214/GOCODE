66349294b2582a98b07727d4b8259e2d6bfdfbfb
ahao214 <haochen214@163.com>
2021-05-21 15:37:41 +0800

    对数组进行循环移位-蛮力法

--- src/ahao.algorithm/chapter04/demo15/main.go
+++ src/ahao.algorithm/chapter04/demo15/main.go
@@ -7,7 +7,7 @@ import (
 //对数组进行循环移位
 
 //蛮力法
-func rightShift(arr []int, k int) {
+func rightShift1(arr []int, k int) {
 	if arr == nil {
 		fmt.Println("参数不合法")
 		return
@@ -23,10 +23,32 @@ func rightShift(arr []int, k int) {
 	}
 }
 
+func rightShift2(arr []int, k int) {
+	if arr == nil {
+		fmt.Println("参数不合法")
+		return
+	}
+	ll := len(arr)
+	k %= ll
+	for k != 0 {
+		k--
+		t := arr[ll-1]
+		for i := ll - 1; i > 0; i-- {
+			arr[i] = arr[i-1]
+		}
+		arr[0] = t
+	}
+}
+
 func main() {
 	arr := []int{1, 2, 3, 4, 5, 6, 7, 8}
-	rightShift(arr, 4)
+	rightShift1(arr, 4)
 	fmt.Println("蛮力法：")
+	for _, v := range arr {
+		fmt.Print(v, " ")
+	}
+	fmt.Println("--")
+	rightShift2(arr, 4)
 	for _, v := range arr {
 		fmt.Print(v, " ")
 	}
