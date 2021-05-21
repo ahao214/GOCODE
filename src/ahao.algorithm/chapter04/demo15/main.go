686009faefaf873bedd1ebcf1c85683be85932ac
ahao214 <haochen214@163.com>
2021-05-21 15:41:31 +0800

    对数组进行循环移位-翻转法

--- src/ahao.algorithm/chapter04/demo15/main.go
+++ src/ahao.algorithm/chapter04/demo15/main.go
@@ -40,6 +40,29 @@ func rightShift2(arr []int, k int) {
 	}
 }
 
+//翻转法
+func reverse(arr []int, start, end int) {
+	for start < end {
+		tmp := arr[start]
+		arr[start] = arr[end]
+		arr[end] = tmp
+		start++
+		end--
+	}
+}
+
+func rightShift3(arr []int, k int) {
+	if arr == nil {
+		fmt.Println("参数不合法")
+		return
+	}
+	ll := len(arr)
+	k %= ll
+	reverse(arr, 0, ll-k-1)
+	reverse(arr, ll-k, ll-1)
+	reverse(arr, 0, ll-1)
+}
+
 func main() {
 	arr := []int{1, 2, 3, 4, 5, 6, 7, 8}
 	rightShift1(arr, 4)
@@ -53,4 +76,9 @@ func main() {
 		fmt.Print(v, " ")
 	}
 
+	fmt.Println("翻转法")
+	rightShift3(arr, 4)
+	for _, v := range arr {
+		fmt.Print(v, " ")
+	}
 }
