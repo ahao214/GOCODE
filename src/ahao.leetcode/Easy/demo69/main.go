3f44a0e4594b9714fb78779552f29d395ce12ff3
ahao214 <haochen214@163.com>
2021-05-26 23:04:05 +0800

    X的平方根-二分法

--- src/ahao.leetcode/Easy/demo69/main.go
+++ src/ahao.leetcode/Easy/demo69/main.go
@@ -13,7 +13,30 @@ func mySqrt(x int) int {
 	return r
 }
 
+//二分法
+func mySqrt2(x int) int {
+	if x == 0 {
+		return 0
+	}
+	left, right, res := 1, x, 0
+	for left <= right {
+		mid := left + ((right - left) >> 1)
+		if mid < x/mid {
+			left = mid + 1
+			res = mid
+		} else if mid == x/mid {
+			return mid
+		} else {
+			right = mid - 1
+		}
+	}
+	return res
+}
+
 func main() {
 	x := 8
+	fmt.Println("牛顿迭代法")
 	fmt.Println(mySqrt(x))
+	fmt.Println("二分法")
+	fmt.Println(mySqrt2(x))
 }
