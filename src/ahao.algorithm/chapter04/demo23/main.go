9f40d8482a94a1fa97ff564559b19b4860b2b7dd
ahao214 <haochen214@163.com>
2021-05-26 14:12:31 +0800

    求两个有序集合的交集-特征法

--- src/ahao.algorithm/chapter04/demo23/main.go
+++ src/ahao.algorithm/chapter04/demo23/main.go
@@ -45,6 +45,37 @@ func getIntersection(l1, l2 []*MySet) []*MySet {
 	return result
 }
 
+//特征法
+func getIntersection2(l1, l2 []*MySet) []*MySet {
+	result := make([]*MySet, 0)
+	for i, j := 0, 0; i < len(l1) && j < len(l2); {
+		s1 := l1[i]
+		s2 := l2[j]
+		if s1.Min < s2.Min {
+			if s1.Max < s2.Min {
+				i++
+			} else if s2.Max <= s2.Max {
+				result = append(result, &MySet{s2.Min, s1.Max})
+				i++
+			} else {
+				result = append(result, &MySet{s2.Min, s2.Max})
+				j++
+			}
+		} else if s2.Min <= s2.Max {
+			if s1.Max <= s2.Max {
+				result = append(result, &MySet{s1.Min, s1.Max})
+				i++
+			} else {
+				result = append(result, &MySet{s1.Min, s2.Max})
+				j++
+			}
+		} else {
+			j++
+		}
+	}
+	return result
+}
+
 func main() {
 	l1 := []*MySet{
 		&MySet{4, 8},
@@ -55,6 +86,12 @@ func main() {
 	}
 	fmt.Println("暴力法")
 	result := getIntersection(l1, l2)
+	for _, v := range result {
+		fmt.Println("[", v.Min, ",", v.Max, "]")
+	}
+
+	fmt.Println("特征法")
+	result = getIntersection2(l1, l2)
 	for _, v := range result {
 		fmt.Println("[", v.Min, ",", v.Max, "]")
 	}
