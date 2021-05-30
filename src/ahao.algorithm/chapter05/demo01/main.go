143d4928030d67490bb2bab93d8dfe67045e404b
ahao214 <haochen214@163.com>
2021-05-30 21:11:22 +0800

    求一个字符串的所有排列-非递归方法

--- src/ahao.algorithm/chapter05/demo01/main.go
+++ src/ahao.algorithm/chapter05/demo01/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	. "github.com/isdamir/gotype"
+	"sort"
 )
 
 //求一个字符串的所有排列
@@ -42,8 +43,47 @@ func Reverse(str []rune, begin, end int) {
 }
 
 //方法功能：根据当前字符串的获取下一个组合
+func getNextPermutation(str []rune) bool {
+	end := len(str) - 1
+	cur := end
+	suc := 0
+	for cur != 0 {
+		//从后向前开始遍历字符串
+		suc = cur
+		cur--
+		if str[cur] < str[suc] {
+			//相邻递增的字符,cur指向较小的字符
+			//找出cur后面最小字符tmp
+			tmp := end
+			for str[tmp] < str[cur] {
+				tmp--
+			}
+			//交换cur与tmp
+			SwapRune(str, cur, tmp)
+			//把cur后面的字符串进行翻转
+			Reverse(str, suc, end)
+			return true
+		}
+	}
+	return false
+}
+
+func PermutationMath(s string) {
+	data := []rune(s)
+	sort.Slice(data, func(i, j int) bool {
+		return data[i] < data[j]
+	}) //升序排列字符数组
+	for true {
+		fmt.Println(string(data))
+		if !getNextPermutation(data) {
+			break
+		}
+	}
+}
 
 func main() {
 	fmt.Println("递归方法")
 	PermutaationStr("abc")
+	fmt.Println("非递归方法")
+	PermutationMath("abc")
 }
