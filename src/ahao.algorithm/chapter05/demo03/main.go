b88b7ec3bb09d72c3e59cc6b2374e02867d09ccf
ahao214 <haochen214@163.com>
2021-06-01 19:19:14 +0800

    对字符串进行翻转-直接交换法

--- src/ahao.algorithm/chapter05/demo03/main.go
+++ src/ahao.algorithm/chapter05/demo03/main.go
@@ -18,9 +18,26 @@ func ReverseStr1(str string) string {
 
 }
 
+//直接交换法
+func ReverseStr2(str string) string {
+	ch := []rune(str)
+	for i, j := 0, len(ch)-1; i < j; i, j = i+1, j-1 {
+		ch[i] = ch[i] ^ ch[j]
+		ch[j] = ch[i] ^ ch[j]
+		ch[i] = ch[i] ^ ch[j]
+	}
+	return string(ch)
+
+}
+
 func main() {
 	str := "abcdefg"
 	fmt.Println("临时变量法：")
 	fmt.Printf("字符串%v翻转后为：%v", str, ReverseStr1(str))
 	fmt.Println()
+	fmt.Println("直接交换法")
+	str2 := "abc"
+	fmt.Printf("字符串%v翻转后为：%v", str2, ReverseStr2(str2))
+	fmt.Println()
+
 }
