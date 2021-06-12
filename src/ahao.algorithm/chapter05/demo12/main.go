bc4032e0b373ea217490558424b3e7d8e9a9f2e7
ahao214 <haochen214@163.com>
2021-06-12 09:01:25 +0800

    判断一个字符串是否包含重复字符-空间换时间

--- src/ahao.algorithm/chapter05/demo12/main.go
+++ src/ahao.algorithm/chapter05/demo12/main.go
@@ -19,10 +19,32 @@ func isDup(str string) bool {
 	return false
 }
 
+//空间换时间
+func isDup2(str string) bool {
+	//只需要8个32位的int，8*32=256位
+	flag := make([]int, 8)
+	for _, v := range str {
+		index := int(v) / 32
+		shift := uint(v) % 32
+		if (flag[index] & (1 << shift)) != 0 {
+			return true
+		}
+		flag[index] |= 1 << shift
+	}
+	return false
+}
+
 func main() {
-	str := "GOOD"
+	str := "GODO"
 	fmt.Println("方法一：")
 	if isDup(str) {
+		fmt.Println(str, "中有重复字符")
+	} else {
+		fmt.Println(str, "中没有重复字符")
+	}
+
+	fmt.Println("方法二：")
+	if isDup2(str) {
 		fmt.Println(str, "中有重复字符")
 	} else {
 		fmt.Println(str, "中没有重复字符")
