150d65b4032a8fad721754c503da14876025255a
ahao214 <haochen214@163.com>
2021-06-18 06:38:25 +0800

    判断一个字符串是否由另外一个字符串旋转得到

---
+++ src/ahao.algorithm/chapter05/demo18/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func isSubstring(str1, str2 string) bool {
+	return strings.Index(str1, str2) >= 0
+}
+
+//判断一个字符串是否由另外一个字符串旋转得到
+func main() {
+
+	fmt.Println()
+}
