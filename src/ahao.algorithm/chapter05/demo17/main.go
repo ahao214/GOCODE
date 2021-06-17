b4041b7c2526d538e41c6c8d2eea7324b0f21791
ahao214 <haochen214@163.com>
2021-06-18 06:36:09 +0800

    求解字符串中字典序最大的子序列-逆序遍历法

--- src/ahao.algorithm/chapter05/demo17/main.go
+++ src/ahao.algorithm/chapter05/demo17/main.go
@@ -26,6 +26,31 @@ func getLargestSub(str string) string {
 	return string(largestSub[0:k])
 }
 
+//逆序遍历法
+func getLargestSub2(txt string) string {
+	ll := len(txt)
+	largestSub := make([]byte, ll+1)
+	//最后一个字符一定在子串中
+	largestSub[0] = txt[ll-1]
+	i := ll - 2
+	j := 0
+	//逆序遍历字符串
+	for ; i >= 0; i-- {
+		if txt[i] >= largestSub[j] {
+			j++
+			largestSub[j] = txt[i]
+		}
+	}
+	largestSub[j+1] = ' '
+	//最子串进行逆序遍历
+	for i = 0; i < j; i, j = i+1, j-1 {
+		tmp := largestSub[i]
+		largestSub[i] = largestSub[j]
+		largestSub[j] = tmp
+	}
+	return string(largestSub)
+}
+
 func main() {
 	a := "acbdxmng"
 	fmt.Println("顺序遍历法：")
@@ -35,4 +60,13 @@ func main() {
 	} else {
 		fmt.Println(result)
 	}
+
+	fmt.Println("逆序遍历法：")
+	result = getLargestSub2(a)
+	if result == "" {
+		fmt.Println("字符串为空")
+	} else {
+		fmt.Println(result)
+	}
+
 }
