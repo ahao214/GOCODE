25afa6788a754cc3086b0cf5a4077754a7154981
ahao214 <haochen214@163.com>
2021-06-25 11:50:32 +0800

    判断一个自然数是否是某个数的平方-二分查找法

--- src/ahao.algorithm/chapter06/demo01/main.go
+++ src/ahao.algorithm/chapter06/demo01/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 //直接计算法
 func isPower(n int) bool {
@@ -19,6 +21,28 @@ func isPower(n int) bool {
 	return false
 }
 
+//二分查找法
+func isPower2(n int) bool {
+	if n <= 0 {
+		fmt.Println(n, "不是自然数")
+		return false
+	}
+	low := 0
+	high := n
+	for low < high {
+		mid := (low + high) / 2
+		power := mid * mid
+		if power > n { //大于则在小的范围找
+			high = mid - 1
+		} else if power < n { //小于则在打的范围找
+			low = mid + 1
+		} else {
+			return true
+		}
+	}
+	return false
+}
+
 //判断一个自然数是否是某个数的平方
 func main() {
 	n1 := 15
@@ -31,6 +55,19 @@ func main() {
 	}
 
 	if isPower(n2) {
+		fmt.Println(n2, "是某个自然数的平方")
+	} else {
+		fmt.Println(n2, "不是某个自然数的平方")
+	}
+
+	fmt.Println("二分查找法：")
+	if isPower2(n1) {
+		fmt.Println(n1, "是某个自然数的平方")
+	} else {
+		fmt.Println(n1, "不是某个自然数的平方")
+	}
+
+	if isPower2(n2) {
 		fmt.Println(n2, "是某个自然数的平方")
 	} else {
 		fmt.Println(n2, "不是某个自然数的平方")
