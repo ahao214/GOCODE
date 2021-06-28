a20954e2ad5f899cff8c7fe23583af4b38c901c7
ahao214 <haochen214@163.com>
2021-06-28 11:29:31 +0800

    用++实现减法操作

--- src/ahao.algorithm/chapter06/demo04/main.go
+++ src/ahao.algorithm/chapter06/demo04/main.go
@@ -23,8 +23,24 @@ func add(a, b int) int {
 	}
 }
 
+//用++实现减法操作(限制条件：被减数大于减数)
+func minus(a, b int) int {
+	if a < b {
+		fmt.Println("无法使用++实现减法操作")
+		return -1
+	}
+	result := 0
+	for ; b != a; b++ {
+		result++
+	}
+	return result
+}
+
 //如何只使用++操作符实现加减乘除运算
 func main() {
 	fmt.Println("加法：")
 	fmt.Println(add(2, 3))
+	fmt.Println("减法：")
+	fmt.Println(minus(9, 2))
+
 }
