e78f665e5202f3848641ce8023d6224cf214c002
ahao214 <haochen214@163.com>
2021-06-28 11:32:33 +0800

    用++实现乘法操作

--- src/ahao.algorithm/chapter06/demo04/main.go
+++ src/ahao.algorithm/chapter06/demo04/main.go
@@ -36,11 +36,25 @@ func minus(a, b int) int {
 	return result
 }
 
+//用++实现乘法操作(限制条件：两个数都为整数)
+func multi(a, b int) int {
+	if a <= 0 || b <= 0 {
+		fmt.Println("无法使用++实现乘法操作")
+		return -1
+	}
+	res := 0
+	for i := 0; i < b; i++ {
+		res = add(res, a)
+	}
+	return res
+}
+
 //如何只使用++操作符实现加减乘除运算
 func main() {
 	fmt.Println("加法：")
 	fmt.Println(add(2, 3))
 	fmt.Println("减法：")
 	fmt.Println(minus(9, 2))
-
+	fmt.Println("乘法：")
+	fmt.Println(multi(9, 3))
 }
