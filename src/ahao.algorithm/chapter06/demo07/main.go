6f271169fc8d4248948b67945ba788b315e35be9
ahao214 <haochen214@163.com>
2021-07-01 20:28:09 +0800

    按要求比较两个数的大小-二进制方法

--- src/ahao.algorithm/chapter06/demo07/main.go
+++ src/ahao.algorithm/chapter06/demo07/main.go
@@ -14,8 +14,20 @@ func max1(a, b int) int {
 	}
 }
 
+//二进制方法
+func max2(a, b int) int {
+	if (a-b)&(1<<31) != 0 {
+		return b
+	} else {
+		return a
+	}
+}
+
 //按要求比较两个数的大小
 func main() {
 	fmt.Println("绝对值方法：")
 	fmt.Println(max1(5, 6))
+
+	fmt.Println("二进制方法：")
+	fmt.Println(max2(45, 56))
 }
