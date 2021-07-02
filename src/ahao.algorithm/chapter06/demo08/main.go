4c91961473decae79c38878ba83c08d843ac4c93
ahao214 <haochen214@163.com>
2021-07-02 08:47:53 +0800

    求有序数列的第1500个数的值-数字规律法

--- src/ahao.algorithm/chapter06/demo08/main.go
+++ src/ahao.algorithm/chapter06/demo08/main.go
@@ -19,8 +19,17 @@ func serach1(n int) int {
 	return i
 }
 
+//数字规律法
+func serach2(n int) int {
+	a := []int{0, 2, 3, 4, 5, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22, 24, 25, 26, 27, 28, 30}
+	return (n/22)*30 + a[n%22]
+}
+
 //求有序数列的第1500个数的值
 func main() {
 	fmt.Println("蛮力方法")
 	fmt.Println(serach1(1500))
+
+	fmt.Println("数字规律法")
+	fmt.Println(serach2(1500))
 }
