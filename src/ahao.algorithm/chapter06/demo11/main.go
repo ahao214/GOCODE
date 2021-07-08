0a5ead86242f16b2c149dc9e38a774c9da94b390
ahao214 <haochen214@163.com>
2021-07-08 12:29:09 +0800

    找最小的不重复数-从左到右的贪心法

--- src/ahao.algorithm/chapter06/demo11/main.go
+++ src/ahao.algorithm/chapter06/demo11/main.go
@@ -52,6 +52,40 @@ func findMinNonDupNum1(n int) int {
 	return result
 }
 
+//从左到右的贪心法
+func findMinNonDupNum2(n int) int {
+	count := 0
+	mRune := []rune(strconv.Itoa(n + 1))
+	ch := make([]rune, len(mRune)+2)
+	ch[0] = '0'
+	ch[len(ch)-1] = '0'
+	for i := 0; i < len(mRune); i++ {
+		ch[i+1] = mRune[i]
+	}
+	ll := len(ch)
+	i := 2 //从左向右遍历
+	for i < ll {
+		count++
+		if ch[i-1] == ch[i] {
+			ch[i]++      //末尾数字加1
+			carry(ch, i) //处理进位
+			//把下标为i后面的字符串变为0101...串
+			for j := i + 1; j < ll; j++ {
+				if (j-i)%2 == 1 {
+					ch[j] = '0'
+				} else {
+					ch[j] = '1'
+				}
+			}
+		} else {
+			i++
+		}
+	}
+	fmt.Println("循环次数为：", count)
+	result, _ := strconv.Atoi(string(ch))
+	return result
+}
+
 //找最小的不重复数
 func main() {
 	fmt.Println("从右到左的贪心法")
@@ -59,5 +93,11 @@ func main() {
 	fmt.Println(findMinNonDupNum1(1101010))
 	fmt.Println(findMinNonDupNum1(99010))
 	fmt.Println(findMinNonDupNum1(8989))
+
+	fmt.Println("从左到右的贪心法")
+	fmt.Println(findMinNonDupNum2(23345))
+	fmt.Println(findMinNonDupNum2(1101010))
+	fmt.Println(findMinNonDupNum2(99010))
+	fmt.Println(findMinNonDupNum2(8989))
 
 }
