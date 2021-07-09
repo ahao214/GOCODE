152986f0005b02ae8bd9947cf556a976e77d3aa4
ahao214 <haochen214@163.com>
2021-07-09 14:34:29 +0800

    计算一个数的n次方-递归法

--- src/ahao.algorithm/chapter06/demo12/main.go
+++ src/ahao.algorithm/chapter06/demo12/main.go
@@ -27,11 +27,40 @@ func power1(d float64, n int) float64 {
 	return result
 }
 
+//递归法
+func power2(d float64, n int) float64 {
+	if n == 0 {
+		return 1
+	}
+	if n == 1 {
+		return d
+	}
+	tmp := power2(d, Abs(n/2))
+	if n > 0 {
+		if n%2 == 1 { //n为奇数
+			return tmp * tmp * d
+		} else {
+			return tmp * tmp
+		}
+	} else {
+		if n%2 == 1 {
+			return 1 / (tmp * tmp * d)
+		} else {
+			return 1 / (tmp * tmp)
+		}
+	}
+}
+
 //计算一个数的n次方
 func main() {
 	fmt.Println("蛮力法")
 	fmt.Println(power1(2, 3))
 	fmt.Println(power1(-2, 3))
 	fmt.Println(power1(2, -3))
+
+	fmt.Println("递归法")
+	fmt.Println(power2(2, 3))
+	fmt.Println(power2(-2, 3))
+	fmt.Println(power2(2, -3))
 
 }
