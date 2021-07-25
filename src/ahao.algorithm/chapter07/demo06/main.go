75efa36c9638a96b5e265fc317ffdb5432dfc403
ahao214 <haochen214@163.com>
2021-07-25 18:52:30 +0800

    如何组合1，2,5这三个数使其和为100-蛮力法

---
+++ src/ahao.algorithm/chapter07/demo06/main.go
@@ -0,0 +1,30 @@
+package demo06
+
+import (
+	"fmt"
+)
+
+//蛮力法
+func combinationCount(n int) int {
+	count := 0
+	num1 := n
+	num2 := n / 2
+	num3 := n / 5
+	for x := 0; x <= num1; x++ {
+		for y := 0; y <= num2; y++ {
+			for z := 0; z <= num3; z++ {
+				if x+2*y+5*z == n {
+					count++
+				}
+			}
+		}
+	}
+	return count
+}
+
+//如何组合1，2,5这三个数使其和为100
+func main() {
+	fmt.Println("蛮力法：")
+	fmt.Println(combinationCount(100))
+
+}
