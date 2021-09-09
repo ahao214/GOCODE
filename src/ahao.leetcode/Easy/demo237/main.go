9d4ccadc8239ed3ed79cb50a470687106775add4
ahao214 <haochen214@163.com>
2021-09-09 09:28:33 +0800

    237.删除链表中的节点

---
+++ src/ahao.leetcode/Easy/demo237/main.go
@@ -0,0 +1,12 @@
+package main
+
+//237.删除链表中的节点
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteNode(node *ListNode) {
+	node.Val = node.Next.Val
+	node.Next = node.Next.Next
+}
