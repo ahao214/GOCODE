f30aac15c021ca2aa676c0ccbcb980b51856d041
ahao214 <haochen214@163.com>
2021-09-13 09:36:42 +0800

    257. 二叉树的所有路径

---
+++ src/ahao.leetcode/Easy/demo257/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "strconv"
+
+//257. 二叉树的所有路径
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func findTreePath(root *TreeNode) []string {
+	if root == nil {
+		return []string{}
+	}
+	res := []string{}
+	if root.Left == nil && root.Right == nil {
+		return []string{strconv.Itoa(root.Val)}
+	}
+	tmpLeft := findTreePath(root.Left)
+	for i := 0; i < len(tmpLeft); i++ {
+		res = append(res, strconv.Itoa(root.Val)+"->"+tmpLeft[i])
+	}
+
+	tmpRight := findTreePath(root.Right)
+	for i := 0; i < len(tmpRight); i++ {
+		res = append(res, strconv.Itoa(root.Val)+"->"+tmpRight[i])
+	}
+	return res
+}
