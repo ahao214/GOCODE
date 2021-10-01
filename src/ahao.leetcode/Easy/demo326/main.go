07628f5cdcf0824be249534826cbd374f81db88f
ahao214 <haochen214@163.com>
2021-10-01 08:27:59 +0800

    326. 3的幂

---
+++ src/ahao.leetcode/Easy/demo326/main.go
@@ -0,0 +1,15 @@
+package main
+
+//326. 3的幂
+
+//循环
+func isPowerOfThree2(num int) bool {
+	for num >= 3 {
+		if num%3 == 0 {
+			num = num / 3
+		} else {
+			return false
+		}
+	}
+	return num == 1
+}
