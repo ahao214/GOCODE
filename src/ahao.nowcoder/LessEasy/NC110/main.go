6296bdf876b6e3e7f95f31da1f1a1bef1ab79483
ahao214 <haochen214@163.com>
2021-10-14 20:56:10 +0800

    旋转数组

---
+++ src/ahao.nowcoder/LessEasy/NC110/main.go
@@ -0,0 +1,22 @@
+package main
+
+/**
+ * 旋转数组
+ * @param n int整型 数组长度
+ * @param m int整型 右移距离
+ * @param a int整型一维数组 给定数组
+ * @return int整型一维数组
+ */
+func solve(n int, m int, a []int) []int {
+	reverse(a)
+	reverse(a[:m%n])
+	reverse(a[m%n:])
+	return a
+}
+
+func reverse(arr []int) []int {
+	for i := 0; i < len(arr)/2; i++ {
+		arr[i], arr[len(arr)-i-1] = arr[len(arr)-i-1], arr[i]
+	}
+	return arr
+}
