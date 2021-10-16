d65a31bd10dbef459f47064c8c4a82d0c033c5d8
ahao214 <haochen214@163.com>
2021-10-16 13:29:37 +0800

    大小写转换

---
+++ src/ahao.lintcode/LessEasy/lint145/main.go
@@ -0,0 +1,11 @@
+package main
+
+/**
+ * 大小写转换
+ * @param character: a character
+ * @return: a character
+ */
+func lowercaseToUppercase(character byte) byte {
+	//根据ASCII
+	return character - 32
+}
