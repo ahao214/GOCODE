ccb876d3209e83a993a0820231806f407269b22f
ahao214 <haochen214@163.com>
2021-10-16 22:51:17 +0800

    136. 只出现一次的数字

---
+++ src/ahao.leetcode/Easy/demo136/main.go
@@ -0,0 +1,18 @@
+package main
+
+//136. 只出现一次的数字
+func singleNumber(nums []int) int {
+	if nums == nil || len(nums) == 0 {
+		return -1
+	}
+	dic := make(map[int]int)
+	for _, v := range nums {
+		dic[v]++
+	}
+	for k, v := range dic {
+		if v == 1 {
+			return k
+		}
+	}
+	return -1
+}
