83c7f0c464eea2682c6f68fd4d721fccf7bb378b
ahao214 <haochen214@163.com>
2021-10-19 21:41:18 +0800

    53.最大子序和

---
+++ src/ahao.leetcode/Easy/demo53/main.go
@@ -0,0 +1,23 @@
+package main
+
+//53.最大子序和
+func maxSubArray(nums []int) int {
+	if len(nums) < 1 {
+		return 0
+	}
+	dp := make([]int, len(nums))
+	result := nums[0]
+	dp[0] = nums[0]
+	for i := 1; i < len(nums); i++ {
+		dp[i] = max(dp[i-1]+nums[i], nums[i])
+		result = max(dp[i], result)
+	}
+	return result
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
