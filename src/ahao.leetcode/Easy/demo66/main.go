aa59b656b1c03b82ce355654712f7ea6d3c034b8
ahao214 <haochen214@163.com>
2021-10-21 17:14:15 +0800

    66.加一

---
+++ src/ahao.leetcode/Easy/demo66/main.go
@@ -0,0 +1,26 @@
+package main
+
+//66.加一
+func plusOne(digits []int) []int {
+	var result []int
+	addone := 0
+	for i := len(digits) - 1; i >= 0; i-- {
+		digits[i] += addone
+		addone = 0
+		if i == len(digits)-1 {
+			digits[i]++
+		}
+		if digits[i] == 10 {
+			addone = 1
+			digits[i] = digits[i] % 10
+		}
+	}
+	if addone == 1 {
+		result = make([]int, 1)
+		result[0] = 1
+		result = append(result, digits...) //将digits切片中的元素追加到result里面
+	} else {
+		result = digits
+	}
+	return result
+}
