55262efa7bd32fe7d733bd7909b3cd43d128c8de
ahao214 <haochen214@163.com>
2021-10-21 17:14:52 +0800

    NC7 买卖股票的最好时机

--- src/ahao.nowcoder/Easy/NC7/main.go
+++ src/ahao.nowcoder/Easy/NC7/main.go
@@ -1,10 +1,21 @@
 package main
 
+import "math"
+
 /**
  * NC7 买卖股票的最好时机
  * @param prices int整型一维数组
  * @return int整型
  */
 func maxProfit(prices []int) int {
-	// write code here
+	minprice := math.MaxInt64
+	maxprofit := 0
+	for i := 0; i < len(prices); i++ {
+		if prices[i] < minprice {
+			minprice = prices[i]
+		} else if prices[i]-minprice > maxprofit {
+			maxprofit = prices[i] - minprice
+		}
+	}
+	return maxprofit
 }
