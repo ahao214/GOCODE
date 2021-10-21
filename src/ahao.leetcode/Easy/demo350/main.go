4aa271f5814ec2da2a98cc9b7f87c6a3a51215cb
ahao214 <haochen214@163.com>
2021-10-21 19:36:42 +0800

    350. 两个数组的交集 II

---
+++ src/ahao.leetcode/Easy/demo350/main.go
@@ -0,0 +1,17 @@
+package main
+
+//350. 两个数组的交集 II
+func intersect(nums1 []int, nums2 []int) []int {
+	m := map[int]int{}
+	var res []int
+	for _, n := range nums1 {
+		m[n]++
+	}
+	for _, n := range nums2 {
+		if m[n] > 0 {
+			res = append(res, n)
+			m[n]--
+		}
+	}
+	return res
+}
