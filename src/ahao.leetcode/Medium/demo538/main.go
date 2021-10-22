f1e122583068653a05300c6c138e1d0ee27d09ce
ahao214 <haochen214@163.com>
2021-10-22 19:13:10 +0800

    538. 把二叉搜索树转换为累加树

--- src/ahao.leetcode/Medium/demo538/main.go
+++ src/ahao.leetcode/Medium/demo538/main.go
@@ -8,5 +8,20 @@ type TreeNode struct {
 
 //538. 把二叉搜索树转换为累加树
 func convertBST(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+	sum := 0
+	inOrder(root, &sum)
+	return root
+}
 
+func inOrder(root *TreeNode, sum *int) {
+	if root == nil {
+		return
+	}
+	inOrder(root.Right, sum)
+	*sum = root.Val
+	root.Val += *sum
+	inOrder(root.Left, sum)
 }
