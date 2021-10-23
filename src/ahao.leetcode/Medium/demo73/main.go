84df1ea1fc8508f3ebdac1d9469ae24980ee9b48
ahao214 <haochen214@163.com>
2021-10-23 20:45:17 +0800

    73.矩阵置零

--- src/ahao.leetcode/Medium/demo73/main.go
+++ src/ahao.leetcode/Medium/demo73/main.go
@@ -81,3 +81,23 @@ func setZeroesT(matrix [][]int) {
 		}
 	}
 }
+
+func setZeroess(matrix [][]int) {
+	row := make([]bool, len(matrix))
+	col := make([]bool, len(matrix[0]))
+	for i, r := range matrix {
+		for j, v := range r {
+			if v == 0 {
+				row[i] = true
+				col[j] = true
+			}
+		}
+	}
+	for i, r := range matrix {
+		for j := range r {
+			if row[i] || col[j] {
+				r[j] = 0
+			}
+		}
+	}
+}
