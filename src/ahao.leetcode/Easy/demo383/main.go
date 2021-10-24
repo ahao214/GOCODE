4c74645a1049006e790626cc616fd8cfd38d4682
ahao214 <haochen214@163.com>
2021-10-24 19:43:07 +0800

    383. 赎金信

---
+++ src/ahao.leetcode/Easy/demo383/main.go
@@ -0,0 +1,26 @@
+package main
+
+//383. 赎金信
+func canConstruct(ransomNote string, magazine string) bool {
+	if len(ransomNote) > len(magazine) {
+		return false
+	}
+	//遍历杂志里面英文字母出现的次数
+	m := make(map[byte]int)
+	for i := 0; i < len(magazine); i++ {
+		m[magazine[i]]++
+	}
+	//遍历信件里面的英文字母的个数
+	for i := 0; i < len(ransomNote); i++ {
+		if num, ok := m[ransomNote[i]]; ok {
+			if num >= 1 {
+				m[ransomNote[i]]--
+			} else {
+				return false
+			}
+		} else {
+			return false
+		}
+	}
+	return true
+}
