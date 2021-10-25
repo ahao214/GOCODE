eadc3792f1142a13438a3d7fed94869cfe692889
ahao214 <haochen214@163.com>
2021-10-25 12:49:03 +0800

    141.环形链表

---
+++ src/ahao.leetcode/Easy/demo141/main.go
@@ -0,0 +1,24 @@
+package demo141
+
+//141.环形链表
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func hasCycle(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return false
+	}
+	slow := head
+	fast := head
+	for fast.Next != nil && fast.Next.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+		if fast == slow {
+			return true
+		}
+	}
+	return false
+}
