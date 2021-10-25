429f17b67a6e694150e1a478054493e2328bb408
ahao214 <haochen214@163.com>
2021-10-25 20:29:24 +0800

    235. 二叉搜索树的最近公共祖先

---
+++ src/ahao.leetcode/Easy/demo235/main.go
@@ -0,0 +1,18 @@
+package main
+
+//235. 二叉搜索树的最近公共祖先
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root.Val > p.Val && root.Val > q.Val {
+		return lowestCommonAncestor(root.Left, p, q)
+	}
+	if root.Val < p.Val && root.Val < q.Val {
+		return lowestCommonAncestor(root.Right, p, q)
+	}
+	return root
+}
