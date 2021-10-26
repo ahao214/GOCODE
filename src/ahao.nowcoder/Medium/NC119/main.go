bbdf3d20a0b83eb57be9cb8eb9a949356dc6f6ce
ahao214 <haochen214@163.com>
2021-10-26 20:14:04 +0800

    NC119 最小的K个数

---
+++ src/ahao.nowcoder/Medium/NC119/main.go
@@ -0,0 +1,15 @@
+package main
+
+/**
+ * NC119 最小的K个数
+ * @param input int整型一维数组
+ * @param k int整型
+ * @return int整型一维数组
+ */
+func GetLeastNumbers_Solution(input []int, k int) []int {
+	res := []int{}
+	if input == nil || len(input) == 0 {
+		return res
+	}
+	return res
+}
