6af04ac8521b7033c52d970dd27f106adbc17b4a
ahao214 <haochen214@163.com>
2021-10-27 21:44:28 +0800

    845. 数组中的最长山脉

---
+++ src/ahao.leetcode/Medium/demo845/main.go
@@ -0,0 +1,33 @@
+package main
+
+//845. 数组中的最长山脉
+func longestMountain(A []int) int {
+	left, right, res, isAscending := 0, 0, 0, true
+	for left < len(A) {
+		if right+1 < len(A) && ((isAscending == true && A[right+1] > A[left] && A[right+1] > A[right]) || (right != left && A[right+1] < A[right])) {
+			if A[right+1] < A[right] {
+				isAscending = false
+			}
+			right++
+		} else {
+			if right != left && isAscending == false {
+				res = max(res, right-left+1)
+			}
+			left++
+			if right < left {
+				right = left
+			}
+			if right == left {
+				isAscending = true
+			}
+		}
+	}
+	return res
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
