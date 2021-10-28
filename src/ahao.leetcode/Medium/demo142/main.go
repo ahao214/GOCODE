3d26a1fd396ba150d0074e8f5fc0f5bacbbab41f
ahao214 <haochen214@163.com>
2021-10-28 20:06:55 +0800

    142. 环形链表 II

---
+++ src/ahao.leetcode/Medium/demo142/main.go
@@ -0,0 +1,30 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+//142. 环形链表 II
+func detectCycle(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			break
+		}
+	}
+	if fast == nil || fast.Next == nil {
+		return nil
+	}
+	third := head
+	for third != slow {
+		slow = slow.Next
+		third = third.Next
+	}
+	return slow
+}
