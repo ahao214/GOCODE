3cb95d42383ef3dd90ef40efcef36d944d40dea1
ahao214 <haochen214@163.com>
2021-10-29 02:35:55 +0800

    NC140 排序

--- src/ahao.nowcoder/Medium/NC140/main.go
+++ src/ahao.nowcoder/Medium/NC140/main.go
@@ -19,3 +19,38 @@ func MySort(arr []int) []int {
 	}
 	return arr
 }
+
+func MySortQ(arr []int) []int {
+	return sort(arr, 0, len(arr)-1)
+}
+
+func sort(arr []int, low, high int) []int {
+	if low >= high {
+		return arr
+	}
+
+	i := low
+	j := high
+	var index int
+	index = arr[i]
+	for i < j {
+		for i < j && arr[j] >= index {
+			j--
+		}
+		if i < j {
+			arr[i] = arr[j]
+			i++
+		}
+		for i < j && arr[i] < index {
+			i++
+		}
+		if i < j {
+			arr[j] = arr[i]
+			j--
+		}
+	}
+	arr[i] = index
+	arr = sort(arr, low, i-1)
+	arr = sort(arr, i+1, high)
+	return arr
+}
