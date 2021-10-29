3177fe8ebccc0ddc6c77a2c8db56c3acff71a3d4
ahao214 <haochen214@163.com>
2021-10-29 17:22:18 +0800

    101. 对称二叉树

---
+++ src/ahao.leetcode/Easy/demo101/main.go
@@ -0,0 +1,23 @@
+package main
+
+//101. 对称二叉树
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSymmetric(root *TreeNode) bool {
+	return isEqual(root.Left, root.Right)
+}
+func isEqual(left *TreeNode, right *TreeNode) bool {
+	if left == nil && right == nil {
+		return true
+	}
+	if left == nil || right == nil || left.Val != right.Val {
+		return false
+	}
+	isLeftEqual := isEqual(left.Left, right.Right)
+	isRightEqual := isEqual(left.Right, right.Left)
+	return isLeftEqual && isRightEqual
+}
