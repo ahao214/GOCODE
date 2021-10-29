ad686c440983896e08e3b6ca580591f75eac3d96
ahao214 <haochen214@163.com>
2021-10-29 19:23:21 +0800

    120. 三角形最小路径和

--- src/ahao.leetcode/Medium/demo120/main.go
+++ src/ahao.leetcode/Medium/demo120/main.go
@@ -2,5 +2,20 @@ package main
 
 //120. 三角形最小路径和
 func minimumTotal(triangle [][]int) int {
+	if triangle == nil {
+		return 0
+	}
+	for row := len(triangle) - 2; row >= 0; row-- {
+		for col := 0; col < len(triangle[row]); col++ {
+			triangle[row][col] += min(triangle[row+1][col], triangle[row+1][col+1])
+		}
+	}
+	return triangle[0][0]
+}
 
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
