552b884d19cc5cfd87676934bb5f03c8a8affbc0
ahao214 <haochen214@163.com>
2021-10-30 16:42:20 +0800

    283. 移动零

--- src/ahao.leetcode/Easy/demo283/main.go
+++ src/ahao.leetcode/Easy/demo283/main.go
@@ -22,6 +22,30 @@ func moveZero(nums []int) {
 	}
 }
 
+func moveZeros(nums []int) {
+	if nums == nil || len(nums) == 0 {
+		return
+	}
+	left := -1
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == 0 {
+			left = i
+			break
+		}
+	}
+	if left < 0 {
+		return
+	}
+	for i := left; i < len(nums); i++ {
+		if nums[i] != 0 && nums[left] == 0 {
+			tmp := nums[i]
+			nums[i] = nums[left]
+			nums[left] = tmp
+			left++
+		}
+	}
+}
+
 func main() {
 	nums := []int{0, 1, 2, 0, 3}
 	moveZero(nums)
