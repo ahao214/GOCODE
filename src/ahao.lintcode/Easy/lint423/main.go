68e77bc1bc80345c91bbd130f6a467bf28694f2f
ahao214 <haochen214@163.com>
2021-11-01 15:54:03 +0800

    423 · 有效的括号序列

---
+++ src/ahao.lintcode/Easy/lint423/main.go
@@ -0,0 +1,25 @@
+package main
+
+/**
+ * 423 · 有效的括号序列
+ * @param s: A string
+ * @return: whether the string is a valid parentheses
+ */
+func isValidParentheses(s string) bool {
+	if len(s) == 0 {
+		return true
+	}
+	stack := make([]rune, 0)
+	for _, v := range s {
+		if (v == '[') || (v == '(') || (v == '{') {
+			stack = append(stack, v)
+		} else if ((v == ']') && len(stack) > 0 && stack[len(stack)-1] == '[') ||
+			((v == ')') && len(stack) > 0 && stack[len(stack)-1] == '(') ||
+			((v == '}') && len(stack) > 0 && stack[len(stack)-1] == '{') {
+			stack = stack[:len(stack)-1]
+		} else {
+			return false
+		}
+	}
+	return len(stack) == 0
+}
