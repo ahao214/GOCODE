d0e56312f8856c40dbe52d16f56074d1059eede7
ahao214 <haochen214@163.com>
2021-11-02 00:23:24 +0800

    27. 移除元素

---
+++ src/ahao.leetcode/Easy/demo27/main.go
@@ -0,0 +1,16 @@
+package main
+
+//27. 移除元素
+func removeElement(nums []int, val int) int {
+	if nums == nil || len(nums) == 0 {
+		return 0
+	}
+	res := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
+			nums[res] = nums[i]
+			res += 1
+		}
+	}
+	return res
+}
