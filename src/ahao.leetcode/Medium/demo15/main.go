2ccf2b48068f94385945199976df69f7c566e5ce
ahao214 <haochen214@163.com>
2021-11-02 00:31:55 +0800

    15. 三数之和

--- src/ahao.leetcode/Medium/demo15/main.go
+++ src/ahao.leetcode/Medium/demo15/main.go
@@ -41,3 +41,41 @@ func threeSum(nums []int) [][]int {
 	}
 	return result
 }
+
+func threeSum2(nums []int) [][]int {
+	res := [][]int{}
+	if nums == nil || len(nums) == 0 {
+		return res
+	}
+
+	sort.Ints(nums)
+	for i := 0; i < len(nums)-2; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		left, right := i+1, len(nums)-1
+		for left < right {
+			sum := nums[i] + nums[left] + nums[right]
+			if sum == 0 {
+				tmp := make([]int, 3)
+				tmp[0] = nums[i]
+				tmp[1] = nums[left]
+				tmp[2] = nums[right]
+				res = append(res, tmp)
+				for left < right && nums[left] == nums[left+1] {
+					left += 1
+				}
+				for left < right && nums[right] == nums[right-1] {
+					right -= 1
+				}
+				left += 1
+				right -= 1
+			} else if sum < 0 {
+				left += 1
+			} else {
+				right -= 1
+			}
+		}
+	}
+	return res
+}
