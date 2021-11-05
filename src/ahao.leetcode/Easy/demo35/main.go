0e48c41696d9974a80d343834f6c18a124bfac8e
ahao214 <haochen214@163.com>
2021-11-05 17:54:30 +0800

    35.搜索插入位置

---
+++ src/ahao.leetcode/Easy/demo35/main.go
@@ -0,0 +1,11 @@
+package main
+
+//35.搜索插入位置
+func searchInsert(nums []int, target int) int {
+	for i := 0; i < len(nums); i++ {
+		if nums[i] >= target {
+			return i
+		}
+	}
+	return len(nums)
+}
