292610ef6cccdb9b48614ff8effd1831748b4852
ahao214 <haochen214@163.com>
2021-11-06 01:04:43 +0800

    268. 丢失的数字

--- src/ahao.leetcode/Easy/demo268/main.go
+++ src/ahao.leetcode/Easy/demo268/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "sort"
+
 //268. 丢失的数字
 func missingNumber(nums []int) int {
 	xor, i := 0, 0
@@ -7,4 +9,15 @@ func missingNumber(nums []int) int {
 		xor = xor ^ i ^ nums[i]
 	}
 	return xor ^ i
+}
+
+//排序方法
+func missingNumber2(arr []int) int {
+	sort.Ints(arr)
+	for i, n := range arr {
+		if n != i {
+			return i
+		}
+	}
+	return len(arr)
 }
