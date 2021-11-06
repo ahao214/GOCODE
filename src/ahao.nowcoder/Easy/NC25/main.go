38497147cb7b6a6b53b16b366a7b66098ba194dc
ahao214 <haochen214@163.com>
2021-11-07 00:53:46 +0800

    NC25 删除有序链表中重复的元素-1

---
+++ src/ahao.nowcoder/Easy/NC25/main.go
@@ -0,0 +1,25 @@
+package main
+
+//NC25 删除有序链表中重复的元素-1
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	cur := head
+	if head == nil {
+		return nil
+	}
+	if head.Next == nil {
+		return head
+	}
+	for cur.Next != nil {
+		if cur.Next.Val == cur.Val {
+			cur.Next = cur.Next.Next
+		} else {
+			cur = cur.Next
+		}
+	}
+	return head
+}
