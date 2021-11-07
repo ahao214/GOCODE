71de58d290b72607c9fd3069492e87eaa332e693
ahao214 <haochen214@163.com>
2021-11-08 01:25:18 +0800

    345. 反转字符串中的元音字母

--- src/ahao.leetcode/Easy/demo345/main.go
+++ src/ahao.leetcode/Easy/demo345/main.go
@@ -23,3 +23,40 @@ func isVowel(s byte) bool {
 	return s == 'a' || s == 'e' || s == 'i' || s == 'o' || s == 'u' || s == 'A' ||
 		s == 'E' || s == 'I' || s == 'O' || s == 'U'
 }
+
+func reverseVowels2(s string) string {
+	sByte := []byte(s)
+	if sByte == nil || len(sByte) <= 1 {
+		return s
+	}
+	dic := make(map[byte]bool)
+	dic['a'] = true
+	dic['e'] = true
+	dic['i'] = true
+	dic['o'] = true
+	dic['u'] = true
+	dic['A'] = true
+	dic['E'] = true
+	dic['I'] = true
+	dic['O'] = true
+	dic['U'] = true
+	left, right := 0, len(sByte)-1
+	for left < right {
+		_, leftOK := dic[sByte[left]]
+		if !leftOK {
+			left++
+		}
+		_, rightOK := dic[sByte[right]]
+		if !rightOK {
+			right--
+		}
+		if leftOK && rightOK {
+			tmp := sByte[left]
+			sByte[left] = sByte[right]
+			sByte[right] = tmp
+			left++
+			right--
+		}
+	}
+	return string(sByte)
+}
