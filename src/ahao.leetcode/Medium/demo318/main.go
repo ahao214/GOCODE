02c0d94068e62acb953485493f8732d4ef2f5b76
ahao214 <haochen214@163.com>
2021-11-17 14:55:54 +0800

    318. 最大单词长度乘积

--- src/ahao.leetcode/Medium/demo318/main.go
+++ src/ahao.leetcode/Medium/demo318/main.go
@@ -22,3 +22,24 @@ func maxProduct(words []string) int {
 	}
 	return maxProduct
 }
+
+func maxProduct2(words []string) (ans int) {
+	masks := map[int]int{}
+	for _, word := range words {
+		mask := 0
+		for _, ch := range word {
+			mask |= 1 << (ch - 'a')
+		}
+		if len(word) > masks[mask] {
+			masks[mask] = len(word)
+		}
+	}
+	for x, lenX := range masks {
+		for y, lenY := range masks {
+			if x&y == 0 && lenX*lenY > ans {
+				ans = lenX * lenY
+			}
+		}
+	}
+	return
+}
