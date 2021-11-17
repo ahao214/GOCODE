db783a27538c49cdb1f4185515611309d0c8dd49
ahao214 <haochen214@163.com>
2021-11-17 14:37:21 +0800

    NC192 二叉树的后序遍历

--- src/ahao.nowcoder/Easy/100/NC192/main.go
+++ src/ahao.nowcoder/Easy/100/NC192/main.go
@@ -6,3 +6,17 @@ type TreeNode struct {
 	Left  *TreeNode
 	Right *TreeNode
 }
+
+func postorderTraversal(root *TreeNode) []int {
+	var result []int
+	postOrder(root, &result)
+	return result
+}
+
+func postOrder(root *TreeNode, result *[]int) {
+	if root != nil {
+		postOrder(root.Left, result)
+		postOrder(root.Right, result)
+		*result = append(*result, root.Val)
+	}
+}
