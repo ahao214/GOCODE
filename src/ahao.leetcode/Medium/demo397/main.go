9e65b85a2c282c5f7d6bb41eaba79b8eb643a3bf
ahao214 <haochen214@163.com>
2021-11-19 17:35:18 +0800

    397. 整数替换-记忆化搜索

--- src/ahao.leetcode/Medium/demo397/main.go
+++ src/ahao.leetcode/Medium/demo397/main.go
@@ -15,6 +15,30 @@ func integerReplacement(n int) int {
 	return 2 + min(integerReplacement(n/2), integerReplacement(n/2+1))
 }
 
+/*
+记忆化搜索
+*/
+var dic map[int]int
+
+func intReplacement(n int) (res int) {
+	if n == 1 {
+		return 0
+	}
+	if res, ok := dic[n]; ok {
+		return res
+	}
+	defer func() { dic[n] = res }()
+	if n%2 == 0 {
+		return 1 + intReplacement(n/2)
+	}
+	return 2 + min(intReplacement(n/2), intReplacement(n/2+1))
+}
+
+func integerReplacement2(n int) int {
+	dic = map[int]int{}
+	return intReplacement(n)
+}
+
 func min(a, b int) int {
 	if a > b {
 		return a
