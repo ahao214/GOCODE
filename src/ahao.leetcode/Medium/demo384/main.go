fe26005e8c23b9af01f78ebe9b488b585412f061
ahao214 <haochen214@163.com>
2021-11-22 13:31:22 +0800

    384.打乱数组-Fisher-Yates洗牌算法

--- src/ahao.leetcode/Medium/demo384/main.go
+++ src/ahao.leetcode/Medium/demo384/main.go
@@ -28,3 +28,26 @@ func (s *Solution) Shuffle() []int {
 	s.nums = shuffled
 	return s.nums
 }
+
+//Fisher-Yates洗牌算法
+type Solution2 struct {
+	nums2, original2 []int
+}
+
+func Constructor2(nums2 []int) Solution2 {
+	return Solution2{nums2, append([]int(nil), nums2...)}
+}
+
+func (s *Solution2) Reset2() []int {
+	copy(s.nums2, s.original2)
+	return s.nums2
+}
+
+func (s *Solution2) Shuffle2() []int {
+	n := len(s.nums2)
+	for i := range s.nums2 {
+		j := i + rand.Intn(n-i)
+		s.nums2[i], s.nums2[j] = s.nums2[j], s.nums2[i]
+	}
+	return s.nums2
+}
