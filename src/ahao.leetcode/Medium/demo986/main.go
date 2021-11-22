337c32be8b0142665f25198cd3ce90788bdc760f
ahao214 <haochen214@163.com>
2021-11-22 20:05:41 +0800

    986. 区间列表的交集

---
+++ src/ahao.leetcode/Medium/demo986/main.go
@@ -0,0 +1,50 @@
+package main
+
+//986. 区间列表的交集
+func intervalIntersection(firstList [][]int, secondList [][]int) [][]int {
+	res := [][]int{}
+	ans := []int{}
+	if firstList == nil || len(firstList) == 0 || secondList == nil || len(secondList) == 0 {
+		return res
+	}
+	i := 0
+	j := 0
+	firstLen := len(firstList)
+	secondLen := len(secondList)
+	for i < firstLen && j < secondLen {
+		start1 := firstList[i][0]
+		end1 := firstList[i][1]
+		start2 := secondList[i][0]
+		end2 := secondList[i][1]
+
+		//选择开始时间晚的
+		start := max(start1, start2)
+		////选择结束时间早的
+		end := min(end1, end2)
+		if start < end {
+			ans = append(ans, start)
+			ans = append(ans, end)
+			res = append(res)
+		}
+		if end1 <= end2 {
+			i++
+		} else {
+			j++
+		}
+	}
+	return res
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
