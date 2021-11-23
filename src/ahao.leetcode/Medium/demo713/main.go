fb9c47743285b2a3bc258bac1fa9b43c3e4d4f78
ahao214 <haochen214@163.com>
2021-11-23 19:29:29 +0800

    713. 乘积小于K的子数组

---
+++ src/ahao.leetcode/Medium/demo713/main.go
@@ -0,0 +1,24 @@
+package main
+
+//713. 乘积小于K的子数组
+func numSubarrayProductLessThanK(nums []int, k int) int {
+	if len(nums) == 0 || k == 0 {
+		return 0
+	}
+
+	res, left, right, prod := 0, 0, 0, 1
+	for left < len(nums) {
+		if right < len(nums) && prod*nums[right] < k {
+			prod = prod * nums[right]
+			right++
+		} else if left == right {
+			left++
+			right++
+		} else {
+			res += right - left
+			prod = prod / nums[left]
+			left++
+		}
+	}
+	return res
+}
