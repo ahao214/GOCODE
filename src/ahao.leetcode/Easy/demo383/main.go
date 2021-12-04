38de4a9da625d969e2004385bb9869b89561e3cd
ahao214 <haochen214@163.com>
2021-12-04 18:16:24 +0800

    383. 赎金信

--- src/ahao.leetcode/Easy/demo383/main.go
+++ src/ahao.leetcode/Easy/demo383/main.go
@@ -24,3 +24,21 @@ func canConstruct(ransomNote string, magazine string) bool {
 	}
 	return true
 }
+
+func canConstruct2(ransomNote string, magazine string) bool {
+	if len(ransomNote) > len(magazine) {
+		return false
+	}
+
+	cnt := [26]int{}
+	for _, ch := range magazine {
+		cnt[ch-'a']++
+	}
+	for _, ch := range ransomNote {
+		cnt[ch-'a']--
+		if cnt[ch-'a'] < 0 {
+			return false
+		}
+	}
+	return true
+}
