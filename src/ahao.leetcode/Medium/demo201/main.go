29fafd7f9c3322e7a865f58b1debec0d1e54ca29
ahao214 <haochen214@163.com>
2021-12-07 01:21:02 +0800

    201. 数字范围按位与

--- src/ahao.leetcode/Medium/demo201/main.go
+++ src/ahao.leetcode/Medium/demo201/main.go
@@ -7,3 +7,12 @@ func rangeBitwiseAnd(left int, right int) int {
 	}
 	return right
 }
+
+func rangeBitwiseAnd2(left int, right int) int {
+	shift := 0
+	for left < right {
+		left, right = left>>1, right>>1
+		shift++
+	}
+	return right << shift
+}
