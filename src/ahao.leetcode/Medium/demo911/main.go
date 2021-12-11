abb631a78d5bde128b3cdc9ebc1047806742c166
ahao214 <haochen214@163.com>
2021-12-11 22:12:37 +0800

    911. 在线选举

--- src/ahao.leetcode/Medium/demo911/main.go
+++ src/ahao.leetcode/Medium/demo911/main.go
@@ -24,3 +24,29 @@ func Constructor(persons, times []int) TopVotedCandidate {
 func (c *TopVotedCandidate) Q(t int) int {
 	return c.tops[sort.SearchInts(c.times, t+1)-1]
 }
+
+// TopVotedCandidate define
+type TopVotedCandidate2 struct {
+	persons []int
+	times   []int
+}
+
+func Constructor911(persons []int, times []int) TopVotedCandidate2 {
+	leaders, votes := make([]int, len(persons)), make([]int, len(persons))
+	leader := persons[0]
+	for i := 0; i < len(persons); i++ {
+		p := persons[i]
+		votes[p]++
+		if votes[p] >= votes[leader] {
+			leader = p
+		}
+		leaders[i] = leader
+	}
+	return TopVotedCandidate2{persons: leaders, times: times}
+}
+
+// Q define
+func (tvc *TopVotedCandidate2) Q2(t int) int {
+	i := sort.Search(len(tvc.times), func(p int) bool { return tvc.times[p] > t })
+	return tvc.persons[i-1]
+}
