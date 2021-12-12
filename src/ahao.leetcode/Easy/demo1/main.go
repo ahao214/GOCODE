59c0483fd444bb84b924da745013ac6f7b014a84
ahao214 <haochen214@163.com>
2021-12-12 22:38:12 +0800

    1.两数之和

--- src/ahao.leetcode/Easy/demo1/main.go
+++ src/ahao.leetcode/Easy/demo1/main.go
@@ -1,6 +1,6 @@
 package main
 
-//两数之和
+//1.两数之和
 //在数组中找到 2 个数之和等于给定值的数字，结果返回 2 个数字在数组中的下标
 //输入：nums = [2, 7, 11, 15], target = 9,
 //解释：nums[0] + nums[1] = 2 + 7 = 9,
@@ -15,4 +15,18 @@ func twoSum(nums []int, target int) []int {
 		m[nums[i]] = i
 	}
 	return nil
+}
+
+func twoNum2(nums []int, target int) []int {
+	res := []int{-1, -1}
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				res[0] = i
+				res[1] = j
+				return res
+			}
+		}
+	}
+	return res
 }
