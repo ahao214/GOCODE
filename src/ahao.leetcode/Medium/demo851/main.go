71550537ac33b7f8af9de907cbdad9a5835e5c85
ahao214 <haochen214@163.com>
2021-12-15 13:48:48 +0800

    851. 喧闹和富有-拓扑排序

--- src/ahao.leetcode/Medium/demo851/main.go
+++ src/ahao.leetcode/Medium/demo851/main.go
@@ -32,3 +32,39 @@ func loudAndRich(richer [][]int, quiet []int) []int {
 	}
 	return ans
 }
+
+//拓扑排序
+func loudAndRich1(richer [][]int, quiet []int) []int {
+	n := len(quiet)
+	g := make([][]int, n)
+	inDeg := make([]int, n)
+	for _, r := range richer {
+		g[r[0]] = append(g[r[0]], r[1])
+		inDeg[r[1]]++
+	}
+
+	ans := make([]int, n)
+	for i := range ans {
+		ans[i] = i
+	}
+	q := make([]int, 0, n)
+	for i, deg := range inDeg {
+		if deg == 0 {
+			q = append(q, i)
+		}
+	}
+	for len(q) > 0 {
+		x := q[0]
+		q = q[1:]
+		for _, y := range g[x] {
+			if quiet[ans[x]] < quiet[ans[y]] {
+				ans[y] = ans[x] // 更新 x 的邻居的答案
+			}
+			inDeg[y]--
+			if inDeg[y] == 0 {
+				q = append(q, y)
+			}
+		}
+	}
+	return ans
+}
