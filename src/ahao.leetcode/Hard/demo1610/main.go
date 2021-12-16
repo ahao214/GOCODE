b6ff00c3b4bdddc6ff75d782586b9f3e7926b4dc
ahao214 <haochen214@163.com>
2021-12-16 16:55:59 +0800

    1610. 可见点的最大数目-滑动窗口

--- src/ahao.leetcode/Hard/demo1610/main.go
+++ src/ahao.leetcode/Hard/demo1610/main.go
@@ -35,3 +35,35 @@ func visiblePoints(points [][]int, angle int, location []int) int {
 	}
 	return sameCnt + maxCnt
 }
+
+//滑动窗口
+func visiblePoints2(points [][]int, angle int, location []int) int {
+	sameCnt := 0
+	polarDegrees := []float64{}
+	for _, p := range points {
+		if p[0] == location[0] && p[1] == location[1] {
+			sameCnt++
+		} else {
+			polarDegrees = append(polarDegrees, math.Atan2(float64(p[1]-location[1]), float64(p[0]-location[0])))
+		}
+	}
+	sort.Float64s(polarDegrees)
+
+	n := len(polarDegrees)
+	for _, deg := range polarDegrees {
+		polarDegrees = append(polarDegrees, deg+2*math.Pi)
+	}
+
+	maxCnt := 0
+	right := 0
+	degree := float64(angle) * math.Pi / 180
+	for i, deg := range polarDegrees[:n] {
+		for right < n*2 && polarDegrees[right] <= deg+degree {
+			right++
+		}
+		if right-i > maxCnt {
+			maxCnt = right - i
+		}
+	}
+	return sameCnt + maxCnt
+}
