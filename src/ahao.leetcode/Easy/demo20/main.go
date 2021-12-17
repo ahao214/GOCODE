0e8497134f4f3d00f238606c90435d967f83584f
ahao214 <haochen214@163.com>
2021-12-17 19:24:16 +0800

    20.有效的括号

--- src/ahao.leetcode/Easy/demo20/main.go
+++ src/ahao.leetcode/Easy/demo20/main.go
@@ -26,6 +26,45 @@ func isValid(s string) bool {
 
 }
 
+func isValid2(s string) bool {
+	if len(s) == 0 {
+		return true
+	}
+	stack := []byte{}
+	for i := 0; i < len(s); i++ {
+		if isLeft(s[i]) {
+			stack = append(stack, s[i])
+		} else {
+			if len(stack) > 0 && stack[len(stack)-1] == findLeft(s[i]) {
+				stack = stack[:len(stack)-1]
+			} else {
+				return false
+			}
+		}
+	}
+	return len(stack) == 0
+}
+
+func isLeft(c byte) bool {
+	if c == '(' || c == '[' || c == '{' {
+		return true
+	}
+	return false
+}
+
+func findLeft(c byte) byte {
+	if c == ')' {
+		return '('
+	}
+	if c == ']' {
+		return '['
+	}
+	if c == '}' {
+		return '{'
+	}
+	return ' '
+}
+
 func main() {
 	s := "(){}}}}"
 	fmt.Println(isValid(s))
