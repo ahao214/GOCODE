20358392c874dd2edd6a951174dc4d1e66fdf04c
ahao214 <haochen214@163.com>
2021-12-18 13:25:31 +0800

    419.甲板上的战舰-遍历扫描

---
+++ src/ahao.leetcode/Medium/demo419/main.go
@@ -0,0 +1,24 @@
+package main
+
+//419.甲板上的战舰
+
+//遍历扫描
+func countBattleships(board [][]byte) int {
+	m, n := len(board), len(board[0])
+	ans := 0
+	for i, row := range board {
+		for j, ch := range row {
+			if ch == 'X' {
+				row[j] = '.'
+				for k := j + 1; k < n && row[k] == 'X'; k++ {
+					row[k] = '.'
+				}
+				for k := i + 1; k < m && board[k][j] == 'X'; k++ {
+					board[k][j] = '.'
+				}
+				ans++
+			}
+		}
+	}
+	return ans
+}
