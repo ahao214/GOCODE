0b3d817ae3f598fdcec9803e688b6d8c3b953618
ahao214 <haochen214@163.com>
2021-12-18 21:44:19 +0800

    236. 二叉树的最近公共祖先

--- src/ahao.leetcode/Medium/demo236/main.go
+++ src/ahao.leetcode/Medium/demo236/main.go
@@ -21,3 +21,18 @@ func LowestTreeNode(root, p, q *TreeNode) *TreeNode {
 	}
 	return right
 }
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil || root.Val == p.Val || root.Val == q.Val {
+		return root
+	}
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+	if left == nil {
+		return right
+	}
+	if right == nil {
+		return left
+	}
+	return root
+}
