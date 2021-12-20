97e919521f3b042600d111ab81157caf8a03a5f0
ahao214 <haochen214@163.com>
2021-12-20 17:57:23 +0800

    475.供暖器-排序+双指针

--- src/ahao.leetcode/Medium/demo475/main.go
+++ src/ahao.leetcode/Medium/demo475/main.go
@@ -5,7 +5,7 @@ import (
 	"sort"
 )
 
-//475.供暖期
+//475.供暖器
 
 //排序+二分
 func findRadius(houses, heaters []int) (ans int) {
@@ -25,4 +25,31 @@ func findRadius(houses, heaters []int) (ans int) {
 		}
 	}
 	return
+}
+
+//排序+双指针
+func findRadius2(houses, heaters []int) (ans int) {
+	sort.Ints(houses)
+	sort.Ints(heaters)
+	j := 0
+	for _, house := range houses {
+		dis := abs(house - heaters[j])
+		for j+1 < len(heaters) && abs(house-heaters[j]) >= abs(house-heaters[j+1]) {
+			j++
+			if abs(house-heaters[j]) < dis {
+				dis = abs(house - heaters[j])
+			}
+		}
+		if dis > ans {
+			ans = dis
+		}
+	}
+	return
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
 }
