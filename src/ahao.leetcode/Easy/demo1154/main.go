bc8d99480c2481e4def24da06c1ac8be0bb96e37
ahao214 <haochen214@163.com>
2021-12-21 05:12:02 +0800

    1154. 一年中的第几天

---
+++ src/ahao.leetcode/Easy/demo1154/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "strconv"
+
+//1154. 一年中的第几天
+func dayOfYear(date string) int {
+	year, _ := strconv.Atoi(date[:4])
+	month, _ := strconv.Atoi(date[5:7])
+	day, _ := strconv.Atoi(date[8:])
+
+	days := []int{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+	if year%400 == 0 || (year%4 == 0 && year%100 != 0) {
+		days[1]++
+	}
+
+	ans := day
+	for _, d := range days[:month-1] {
+		ans += d
+	}
+	return ans
+}
