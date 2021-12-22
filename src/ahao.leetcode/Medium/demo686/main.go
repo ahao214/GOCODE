8ac41b32870b0fd4fa6996c0ab71b2bcaa57610d
ahao214 <haochen214@163.com>
2021-12-22 18:37:46 +0800

    686. 重复叠加字符串匹配-KMP算法

--- src/ahao.leetcode/Medium/demo686/main.go
+++ src/ahao.leetcode/Medium/demo686/main.go
@@ -51,3 +51,45 @@ func repeatedStringMatch(a string, b string) int {
 	}
 	return (bn+index-an-1)/an + 2
 }
+
+//方法二：Knuth-Morris-Pratt 算法
+func strStr2(haystack, needle string) int {
+	n, m := len(haystack), len(needle)
+	if m == 0 {
+		return 0
+	}
+	pi := make([]int, m)
+	for i, j := 1, 0; i < m; i++ {
+		for j > 0 && needle[i] != needle[j] {
+			j = pi[j-1]
+		}
+		if needle[i] == needle[j] {
+			j++
+		}
+		pi[i] = j
+	}
+	for i, j := 0, 0; i-j < n; i++ { // b 开始匹配的位置是否超过第一个叠加的 a
+		for j > 0 && haystack[i%n] != needle[j] { // haystack 是循环叠加的字符串，所以取 i % n
+			j = pi[j-1]
+		}
+		if haystack[i%n] == needle[j] {
+			j++
+		}
+		if j == m {
+			return i - m + 1
+		}
+	}
+	return -1
+}
+
+func repeatedStringMatch2(a string, b string) int {
+	an, bn := len(a), len(b)
+	index := strStr2(a, b)
+	if index == -1 {
+		return -1
+	}
+	if an-index >= bn {
+		return 1
+	}
+	return (bn+index-an-1)/an + 2
+}
