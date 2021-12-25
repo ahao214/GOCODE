12e0b464bf5d32202e1ce72f247772971e1670cd
ahao214 <haochen214@163.com>
2021-12-26 00:08:49 +0800

    1078. Bigram 分词

---
+++ src/ahao.leetcode/Easy/demo1078/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "strings"
+
+//1078. Bigram 分词
+func findOcurrences(text, first, second string) (ans []string) {
+	words := strings.Split(text, " ")
+	for i := 2; i < len(words); i++ {
+		if words[i-2] == first && words[i-1] == second {
+			ans = append(ans, words[i])
+		}
+	}
+	return
+}
