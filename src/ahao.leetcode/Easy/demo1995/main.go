9651fa71a254ea62259f2f3899a4d7fdbb73735b
ahao214 <haochen214@163.com>
2021-12-29 07:25:40 +0800

    1995. 统计特殊四元组

--- src/ahao.leetcode/Easy/demo1995/main.go
+++ src/ahao.leetcode/Easy/demo1995/main.go
@@ -15,3 +15,19 @@ func countQuadruplets(nums []int) (ans int) {
 	}
 	return
 }
+
+//使用哈希表存储nums[d]
+func countQuadruplets1(nums []int) (ans int) {
+	cnt := map[int]int{}
+	for c := len(nums) - 2; c >= 2; c-- {
+		cnt[nums[c+1]]++
+		for a, x := range nums[:c] {
+			for _, y := range nums[a+1 : c] {
+				if sum := x + y + nums[c]; cnt[sum] > 0 {
+					ans += cnt[sum]
+				}
+			}
+		}
+	}
+	return
+}
