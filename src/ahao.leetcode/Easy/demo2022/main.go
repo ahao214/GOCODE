9fd0cb95249702c135bb8291735b16ca34e9728c
ahao214 <haochen214@163.com>
2022-01-01 19:40:53 +0800

    2022. 将一维数组转变成二维数组

--- src/ahao.leetcode/Easy/demo2022/main.go
+++ src/ahao.leetcode/Easy/demo2022/main.go
@@ -12,3 +12,18 @@ func construct2DArray(original []int, m, n int) [][]int {
 	}
 	return ans
 }
+
+func construct2DArray2(original []int, m, n int) [][]int {
+	if len(original) != m*n {
+		return nil
+	}
+	res := make([][]int, m)
+
+	left, right := 0, n
+	for i := 0; i < m; i++ {
+		res[i] = original[left:right]
+		left += n
+		right += n
+	}
+	return res
+}
