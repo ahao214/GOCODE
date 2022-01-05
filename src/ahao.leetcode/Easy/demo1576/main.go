e576f320c97d8c7f6ffc2d9af72a133db1bc8c19
ahao214 <haochen214@163.com>
2022-01-05 14:08:50 +0800

    1576. 替换所有的问号

---
+++ src/ahao.leetcode/Easy/demo1576/main.go
@@ -0,0 +1,18 @@
+package main
+
+//1576. 替换所有的问号
+func modifyString(s string) string {
+	res := []byte(s)
+	n := len(res)
+	for i, ch := range res {
+		if ch == '?' {
+			for b := byte('a'); b <= 'c'; b++ {
+				if !(i > 0 && res[i-1] == b || i < n-1 && res[i+1] == b) {
+					res[i] = b
+					break
+				}
+			}
+		}
+	}
+	return string(res)
+}
