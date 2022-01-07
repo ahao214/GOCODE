ae120827997e7943b5e755b14ebe0327deefe402
ahao214 <haochen214@163.com>
2022-01-07 08:37:20 +0800

    290. 单词规律

--- src/ahao.leetcode/Easy/demo290/main.go
+++ src/ahao.leetcode/Easy/demo290/main.go
@@ -30,3 +30,29 @@ func wordPattern(pattern string, str string) bool {
 	}
 	return true
 }
+
+func wordPattern1(pattern string, s string) bool {
+	patterns := strings.Split(pattern, "")
+	strs := strings.Split(s, " ")
+	if len(strs) != len(patterns) {
+		return false
+	}
+	return checkPattern(strs, patterns) && checkPattern(patterns, strs)
+}
+
+func checkPattern(a []string, b []string) bool {
+	dic := make(map[string]string)
+	p := 0
+	for p < len(a) {
+		val, ok := dic[a[p]]
+		if !ok {
+			dic[a[p]] = b[p]
+		} else {
+			if val != b[p] {
+				return false
+			}
+		}
+		p++
+	}
+	return true
+}
