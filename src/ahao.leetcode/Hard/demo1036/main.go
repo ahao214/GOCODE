063aebf3dbf87506b29022287bd403790af01008
ahao214 <haochen214@163.com>
2022-01-11 19:35:21 +0800

    1036. 逃离大迷宫

--- src/ahao.leetcode/Hard/demo1036/main.go
+++ src/ahao.leetcode/Hard/demo1036/main.go
@@ -10,6 +10,8 @@ package main
 
 只有在可以通过一系列的移动从源方格 source 到达目标方格 target 时才返回 true。否则，返回 false。
 */
+
+//方法一：有限步数的广度优先搜索
 type pair struct{ x, y int }
 
 var dirs = []pair{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
@@ -65,4 +67,84 @@ func isEscapePossible(block [][]int, source, target []int) bool {
 
 	res := check(source, target)
 	return res == found || res == valid && check(target, source) != blocked
+}
+
+//方法二：离散化 + 广度优先搜索
+type pair struct{ x, y int }
+
+var dirs = []pair{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
+
+// 离散化 a，返回的哈希表中的键值对分别为 a 中的原始值及其离散化后的对应值
+func discrete(a []int) (map[int]int, int) {
+	sort.Ints(a)
+
+	id := 0
+	if a[0] > 0 {
+		id = 1
+	}
+	mapping := map[int]int{a[0]: id}
+	pre := a[0]
+	for _, v := range a[1:] {
+		if v != pre {
+			if v == pre+1 {
+				id++
+			} else {
+				id += 2
+			}
+			mapping[v] = id
+			pre = v
+		}
+	}
+
+	const boundary int = 1e6
+	if a[len(a)-1] != boundary-1 {
+		id++
+	}
+
+	return mapping, id
+}
+
+func isEscapePossible2(block [][]int, source, target []int) bool {
+	n := len(block)
+	if n < 2 {
+		return true
+	}
+	rows := []int{source[0], target[0]}
+	cols := []int{source[1], target[1]}
+	for _, b := range block {
+		rows = append(rows, b[0])
+		cols = append(cols, b[1])
+	}
+
+	// 离散化行列坐标
+	rMapping, rBound := discrete(rows)
+	cMapping, cBound := discrete(cols)
+
+	grid := make([][]bool, rBound+1)
+	for i := range grid {
+		grid[i] = make([]bool, cBound+1)
+	}
+	for _, b := range block {
+		grid[rMapping[b[0]]][cMapping[b[1]]] = true
+	}
+
+	sx, sy := rMapping[source[0]], cMapping[source[1]]
+	tx, ty := rMapping[target[0]], cMapping[target[1]]
+	grid[sx][sy] = true
+	q := []pair{{sx, sy}}
+	for len(q) > 0 {
+		p := q[0]
+		q = q[1:]
+		for _, d := range dirs {
+			x, y := p.x+d.x, p.y+d.y
+			if 0 <= x && x <= rBound && 0 <= y && y <= cBound && !grid[x][y] {
+				if x == tx && y == ty {
+					return true
+				}
+				grid[x][y] = true
+				q = append(q, pair{x, y})
+			}
+		}
+	}
+	return false
 }
