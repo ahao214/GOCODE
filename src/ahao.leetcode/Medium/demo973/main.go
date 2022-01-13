a6becc51944259055c915ba755450c9c32cf4948
ahao214 <haochen214@163.com>
2022-01-13 09:12:17 +0800

    973. 最接近原点的 K 个点

---
+++ src/ahao.leetcode/Medium/demo973/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "sort"
+
+/*
+973. 最接近原点的 K 个点
+我们有一个由平面上的点组成的列表 points。需要从中找出 K 个距离原点 (0, 0) 最近的点。
+（这里，平面上两点之间的距离是欧几里德距离。）
+你可以按任何顺序返回答案。除了点坐标的顺序之外，答案确保是唯一的。
+
+输入：points = [[1,3],[-2,2]], K = 1
+输出：[[-2,2]]
+解释：
+(1, 3) 和原点之间的距离为 sqrt(10)，
+(-2, 2) 和原点之间的距离为 sqrt(8)，
+由于 sqrt(8) < sqrt(10)，(-2, 2) 离原点更近。
+我们只需要距离原点最近的 K = 1 个点，所以答案就是 [[-2,2]]。
+
+*/
+
+func kClosest(points [][]int, k int) [][]int {
+	sort.Slice(points, func(i, j int) bool {
+		return distance(points[i]) < distance(points[j])
+	})
+	return points[:k]
+}
+
+func distance(point []int) int {
+	return point[0]*point[0] + point[1]*point[1]
+}
