24060fa712b7a3078a176858c4444f5d671a8293
ahao214 <haochen214@163.com>
2022-01-15 09:22:26 +0800

    1716. 计算力扣银行的钱

--- src/ahao.leetcode/Easy/demo1716/main.go
+++ src/ahao.leetcode/Easy/demo1716/main.go
@@ -1,8 +1,7 @@
 package main
 
 /*
-//1716. 计算力扣银行的钱
-
+1716. 计算力扣银行的钱
 Hercy 想要为购买第一辆车存钱。他 每天 都往力扣银行里存钱。
 
 最开始，他在周一的时候存入 1 块钱。从周二到周日，他每天都比前一天多存入 1 块钱。在接下来每一个周一，他都会比 前一个周一 多存入 1 块钱。
@@ -22,4 +21,18 @@ func totalMoney(n int) (ans int) {
 		}
 	}
 	return
+}
+
+func totalMoney1(n int) (ans int) {
+	// 所有完整的周存的钱
+	weekNum := n / 7
+	firstWeekMoney := (1 + 7) * 7 / 2
+	lastWeekMoney := firstWeekMoney + 7*(weekNum-1)
+	weekMoney := (firstWeekMoney + lastWeekMoney) * weekNum / 2
+	// 剩下的不能构成一个完整的周的天数里存的钱
+	dayNum := n % 7
+	firstDayMoney := 1 + weekNum
+	lastDayMoney := firstDayMoney + dayNum - 1
+	dayMoney := (firstDayMoney + lastDayMoney) * dayNum / 2
+	return weekMoney + dayMoney
 }
