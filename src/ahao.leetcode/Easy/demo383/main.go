ccf10916e95d4c1235b4210930ebd56d25e15bea
ahao214 <haochen214@163.com>
2022-01-17 18:43:30 +0800

    383. 赎金信

--- src/ahao.leetcode/Easy/demo383/main.go
+++ src/ahao.leetcode/Easy/demo383/main.go
@@ -1,6 +1,11 @@
 package main
 
-//383. 赎金信
+/*
+383. 赎金信
+给你两个字符串：ransomNote 和 magazine ，判断 ransomNote 能不能由 magazine 里面的字符构成。
+如果可以，返回 true ；否则返回 false 。
+magazine 中的每个字符只能在 ransomNote 中使用一次。
+*/
 func canConstruct(ransomNote string, magazine string) bool {
 	if len(ransomNote) > len(magazine) {
 		return false
@@ -37,6 +42,31 @@ func canConstruct2(ransomNote string, magazine string) bool {
 	for _, ch := range ransomNote {
 		cnt[ch-'a']--
 		if cnt[ch-'a'] < 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func canConstruct3(ransomNote string, magazine string) bool {
+	if len(ransomNote) > len(magazine) {
+		return false
+	}
+	r := []byte(ransomNote)
+	m := []byte(magazine)
+	dic := make(map[byte]int)
+	for i := 0; i < len(m); i++ {
+		dic[m[i]]++
+	}
+	for i := 0; i < len(r); i++ {
+		val, ok := dic[r[i]]
+		if ok {
+			if val == 0 {
+				return false
+			} else {
+				dic[r[i]]--
+			}
+		} else {
 			return false
 		}
 	}
