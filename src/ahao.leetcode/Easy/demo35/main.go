507d2427a222f992eedef9c8c2736b9d3a79de2b
ahao214 <haochen214@163.com>
2022-01-18 14:50:30 +0800

    35.搜索插入位置

--- src/ahao.leetcode/Easy/demo35/main.go
+++ src/ahao.leetcode/Easy/demo35/main.go
@@ -1,6 +1,11 @@
 package main
 
-//35.搜索插入位置
+/*
+35.搜索插入位置
+给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。
+如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+
+*/
 func searchInsert(nums []int, target int) int {
 	for i := 0; i < len(nums); i++ {
 		if nums[i] >= target {
@@ -8,4 +13,22 @@ func searchInsert(nums []int, target int) int {
 		}
 	}
 	return len(nums)
+}
+
+func searchInsert1(nums []int, target int) int {
+	if nums == nil || len(nums) == 0 {
+		return -1
+	}
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] > target {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+	return left
 }
