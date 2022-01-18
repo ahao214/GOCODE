6be60506fef4c9cd54728d82bb40361cff35bc2a
ahao214 <haochen214@163.com>
2022-01-18 12:38:57 +0800

    34. 在排序数组中查找元素的第一个和最后一个位置

--- src/ahao.leetcode/Medium/demo34/main.go
+++ src/ahao.leetcode/Medium/demo34/main.go
@@ -1,6 +1,11 @@
 package main
 
-//34. 在排序数组中查找元素的第一个和最后一个位置
+/*
+34. 在排序数组中查找元素的第一个和最后一个位置
+给定一个按照升序排列的整数数组 nums，和一个目标值 target。
+找出给定目标值在数组中的开始位置和结束位置。
+如果数组中不存在目标值 target，返回 [-1, -1]。
+*/
 func searchRange(nums []int, target int) []int {
 	return []int{searchFirstEqualElement(nums, target), searchLastEqualElement(nums, target)}
 }
@@ -75,4 +80,35 @@ func searchLastLessElement(nums []int, target int) int {
 		}
 	}
 	return -1
+}
+
+func searchRange1(nums []int, target int) []int {
+	res := []int{-1, -1}
+	if nums == nil || len(nums) == 0 {
+		return res
+	}
+	left, right := 0, len(nums)
+	for left < right {
+		mid := left + (right-left)/2
+		if nums[mid] >= target {
+			right = mid
+		} else if nums[mid] < target {
+			left = mid + 1
+		}
+	}
+	if left > len(nums)-1 || nums[left] > target {
+		return res
+	}
+	res[0] = left
+	left, right = 0, len(nums)
+	for left < right {
+		mid := left + (right-left)/2
+		if nums[mid] <= target {
+			left = mid + 1
+		} else if nums[mid] > target {
+			right = mid
+		}
+	}
+	res[1] = right - 1
+	return res
 }
