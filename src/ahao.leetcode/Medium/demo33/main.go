ef8c4dba69d7474ccdd2ff2044ba4305673ef56b
ahao214 <haochen214@163.com>
2022-01-18 14:41:37 +0800

    33. 搜索旋转排序数组

--- src/ahao.leetcode/Medium/demo33/main.go
+++ src/ahao.leetcode/Medium/demo33/main.go
@@ -1,6 +1,16 @@
 package main
 
-//33. 搜索旋转排序数组
+/*
+33. 搜索旋转排序数组
+整数数组 nums 按升序排列，数组中的值 互不相同 。
+
+在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转，
+使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ...,
+nums[k-1]]（下标 从 0 开始 计数）。例如， [0,1,2,4,5,6,7] 在下标 3 处经旋转后可能变为
+[4,5,6,7,0,1,2] 。
+给你 旋转后 的数组 nums 和一个整数 target ，如果 nums 中存在这个目标值 target ，
+则返回它的下标，否则返回 -1 。
+*/
 func search(nums []int, target int) int {
 	if len(nums) == 0 {
 		return -1
@@ -28,6 +38,34 @@ func search(nums []int, target int) int {
 			}
 			if nums[high] == nums[mid] {
 				high--
+			}
+		}
+	}
+	return -1
+}
+
+func search1(nums []int, target int) int {
+	if nums == nil || len(nums) == 0 {
+		return -1
+	}
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			return mid
+		} else if target >= nums[0] {
+			if nums[mid] < target && nums[0] <= nums[mid] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		} else if target < nums[0] {
+			if nums[mid] < target {
+				left = mid + 1
+			} else if nums[mid] >= nums[0] {
+				left = mid + 1
+			} else {
+				right = mid - 1
 			}
 		}
 	}
