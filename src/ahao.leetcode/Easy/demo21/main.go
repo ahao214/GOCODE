f55b35e8d54985a1cb5b5015751f6a546e5d71f6
ahao214 <haochen214@163.com>
2022-01-19 21:48:01 +0800

    21. 合并两个有序链表

--- src/ahao.leetcode/Easy/demo21/main.go
+++ src/ahao.leetcode/Easy/demo21/main.go
@@ -1,6 +1,9 @@
 package demo21
 
-//21. 合并两个有序链表
+/*
+21. 合并两个有序链表
+将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+*/
 type ListNode struct {
 	Val  int
 	Next *ListNode
@@ -19,4 +22,29 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 	}
 	l2.Next = mergeTwoLists(l1, l2.Next)
 	return l2
+}
+
+func mergeTwoLists1(list1 *ListNode, list2 *ListNode) *ListNode {
+	pre := &ListNode{0, nil}
+	cur := pre
+	for list1 != nil && list2 != nil {
+		tmp := list1.Val
+		if list1.Val > list2.Val {
+			tmp = list2.Val
+		}
+		cur.Next = &ListNode{tmp, nil}
+		cur = cur.Next
+		if list1.Val > list2.Val {
+			list2 = list2.Next
+		} else {
+			list1 = list1.Next
+		}
+	}
+	if list1 != nil {
+		cur.Next = list1
+	}
+	if list2 != nil {
+		cur.Next = list2
+	}
+	return pre.Next
 }
