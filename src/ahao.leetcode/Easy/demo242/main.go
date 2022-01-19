62bc630590888e992318dfc46b32b9ad334b1954
ahao214 <haochen214@163.com>
2022-01-19 13:25:59 +0800

    242. 有效的字母异位词

--- src/ahao.leetcode/Easy/demo242/main.go
+++ src/ahao.leetcode/Easy/demo242/main.go
@@ -2,7 +2,12 @@ package main
 
 import "fmt"
 
-//242. 有效的字母异位词
+/*
+242. 有效的字母异位词
+给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+
+注意：若 s 和 t 中每个字符出现的次数都相同，则称 s 和 t 互为字母异位词。
+*/
 func isAnagram(s string, t string) bool {
 	hash := map[rune]int{}
 	for _, value := range s {
@@ -22,4 +27,22 @@ func isAnagram(s string, t string) bool {
 func main() {
 	isTrue := isAnagram("rat", "tar")
 	fmt.Println(isTrue)
+}
+
+func isAnagram1(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+	sByte, tByte := []byte(s), []byte(t)
+	m := make(map[byte]int)
+	for i := 0; i < len(sByte); i++ {
+		m[sByte[i]]++
+	}
+	for i := 0; i < len(tByte); i++ {
+		m[tByte[i]]--
+		if m[tByte[i]] < 0 {
+			return false
+		}
+	}
+	return true
 }
