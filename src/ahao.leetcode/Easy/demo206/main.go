d02b00e4febf2af14ce2fdeb618dc8d69bafa03a
ahao214 <haochen214@163.com>
2022-01-19 21:33:22 +0800

    206.反转链表

--- src/ahao.leetcode/Easy/demo206/main.go
+++ src/ahao.leetcode/Easy/demo206/main.go
@@ -1,6 +1,9 @@
 package main
 
-//206.反转链表
+/*
+206.反转链表
+给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+*/
 type ListNode struct {
 	Val  int
 	Next *ListNode
@@ -14,4 +17,19 @@ func reverseList(head *ListNode) *ListNode {
 	head.Next.Next = head
 	head.Next = nil
 	return newHead
+}
+
+func reverseList1(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	var pre *ListNode
+	cur := head
+	for cur != nil {
+		nxt := cur.Next
+		cur.Next = pre
+		pre = cur
+		cur = nxt
+	}
+	return pre
 }
