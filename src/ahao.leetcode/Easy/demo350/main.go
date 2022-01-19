9d0ce7417836e54d13cfde600b1864eb657026b9
ahao214 <haochen214@163.com>
2022-01-19 13:47:50 +0800

    350. 两个数组的交集 II

--- src/ahao.leetcode/Easy/demo350/main.go
+++ src/ahao.leetcode/Easy/demo350/main.go
@@ -8,6 +8,8 @@ import "sort"
 返回结果中每个元素出现的次数，应与元素在两个数组中都出现的次数一致
 （如果出现次数不一致，则考虑取较小值）。
 可以不考虑输出结果的顺序。
+输入：nums1 = [1,2,2,1], nums2 = [2,2]
+输出：[2,2]
 */
 func intersect(nums1 []int, nums2 []int) []int {
 	m := map[int]int{}
