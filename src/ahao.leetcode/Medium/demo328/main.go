e225341664b7803512e4c6fe5921315cd4397402
ahao214 <haochen214@163.com>
2022-01-20 19:41:33 +0800

    328. 奇偶链表

--- src/ahao.leetcode/Medium/demo328/main.go
+++ src/ahao.leetcode/Medium/demo328/main.go
@@ -1,6 +1,12 @@
 package main
 
-//328. 奇偶链表
+/*
+328. 奇偶链表
+给定一个单链表，把所有的奇数节点和偶数节点分别排在一起。
+请注意，这里的奇数节点和偶数节点指的是节点编号的奇偶性，而不是节点的值的奇偶性。
+
+请尝试使用原地算法完成。你的算法的空间复杂度应为 O(1)，时间复杂度应为 O(nodes)，nodes 为节点总数。
+*/
 type ListNode struct {
 	Val  int
 	Next *ListNode
@@ -27,4 +33,22 @@ func oddEvenList(head *ListNode) *ListNode {
 	even.Next = nil
 	odd.Next = evenHead.Next
 	return oddHead.Next
+}
+
+func oddEvenList1(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	//define odd even evenHead
+	odd, even, evenHead := head, head.Next, head.Next
+	for even != nil && even.Next != nil {
+		oddNext := even.Next
+		odd.Next = oddNext
+		odd = odd.Next
+		evenNext := odd.Next
+		even.Next = evenNext
+		even = even.Next
+	}
+	odd.Next = evenHead
+	return head
 }
