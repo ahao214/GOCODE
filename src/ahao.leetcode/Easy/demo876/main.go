a2e5c64c8614516abc9ca6a4a5ed75c4e71afda2
ahao214 <haochen214@163.com>
2022-01-20 19:22:11 +0800

    876. 链表的中间结点

--- src/ahao.leetcode/Easy/demo876/main.go
+++ src/ahao.leetcode/Easy/demo876/main.go
@@ -1,7 +1,11 @@
 package main
 
-//876. 链表的中间结点
+/*
+876. 链表的中间结点
+给定一个头结点为 head 的非空单链表，返回链表的中间结点。
 
+如果有两个中间结点，则返回第二个中间结点。
+*/
 type ListNode struct {
 	Val  int
 	Next *ListNode
@@ -25,6 +29,21 @@ func middleNode(head *ListNode) *ListNode {
 	}
 	if length%2 == 0 {
 		return slow.Next
+	}
+	return slow
+}
+
+func middleNode1(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+	slow, fast := head, head
+	for fast.Next != nil && fast.Next.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	if fast.Next != nil && fast.Next.Next == nil {
+		slow = slow.Next
 	}
 	return slow
 }
