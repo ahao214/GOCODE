5f17f72639a036f8b71b5055e28dadd98c325d0e
ahao214 <haochen214@163.com>
2022-01-21 22:17:22 +0800

    24. 两两交换链表中的节点

--- src/ahao.leetcode/Medium/demo24/main.go
+++ src/ahao.leetcode/Medium/demo24/main.go
@@ -1,6 +1,10 @@
 package main
 
-//24. 两两交换链表中的节点
+/*
+24. 两两交换链表中的节点
+给你一个链表，两两交换其中相邻的节点，并返回交换后链表的头节点。
+你必须在不修改节点内部的值的情况下完成本题（即，只能进行节点交换）。
+*/
 type ListNode struct {
 	Val  int
 	Next *ListNode
@@ -10,6 +14,26 @@ func swapPairs(head *ListNode) *ListNode {
 	dummy := &ListNode{Next: head}
 	for pt := dummy; pt != nil && pt.Next != nil && pt.Next.Next != nil; {
 		pt, pt.Next, pt.Next.Next, pt.Next.Next.Next = pt.Next, pt.Next.Next, pt.Next.Next.Next, pt.Next
+	}
+	return dummy.Next
+}
+
+func swapPairs1(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+	dummy := &ListNode{-1, head}
+	pre := dummy
+	cur := head
+	for cur != nil && cur.Next != nil {
+		nxt := cur.Next
+		nextNxt := nxt.Next
+
+		pre.Next = nxt
+		nxt.Next = cur
+		cur.Next = nextNxt
+		pre = cur
+		cur = cur.Next
 	}
 	return dummy.Next
 }
