f1646550f38bd3fa9ee25af6223d6dc9de9ba001
ahao214 <haochen214@163.com>
2022-01-22 11:00:37 +0800

    82. 删除排序链表中的重复元素 II

--- src/ahao.leetcode/Medium/demo82/main.go
+++ src/ahao.leetcode/Medium/demo82/main.go
@@ -1,6 +1,12 @@
 package main
 
-//82. 删除排序链表中的重复元素 II
+/*
+82. 删除排序链表中的重复元素 II
+存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除链表中所有存在数字重复情况的节点，
+只保留原始链表中 没有重复出现 的数字。
+
+返回同样按升序排列的结果链表。
+*/
 type ListNode struct {
 	Val  int
 	Next *ListNode
@@ -63,6 +69,5 @@ func deleteDuplicates2(head *ListNode) *ListNode {
 			cur = cur.Next
 		}
 	}
-
 	return dummy.Next
 }
