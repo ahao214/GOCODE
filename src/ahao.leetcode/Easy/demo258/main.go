53b5db367d80fd74bdc2336337e06245f8e6fdf0
ahao214 <haochen214@163.com>
2022-03-09 01:10:36 +0800

    258. 各位相加

--- src/ahao.leetcode/Easy/demo258/main.go
+++ src/ahao.leetcode/Easy/demo258/main.go
@@ -1,20 +1,17 @@
 package main
 
-import "fmt"
+/*
+258. 各位相加
+给定一个非负整数 num，反复将各个位上的数字相加，直到结果为一位数。返回这个结果。
+*/
 
-//524. 通过删除字母匹配到字典里最长单词
 func addDigits(num int) int {
-	for num > 0 {
-		cur := 0
-		for num != 0 {
-			cur += num % 10
-			num /= 10
+	for num >= 10 {
+		sum := 0
+		for ; num > 0; num /= 10 {
+			sum += num % 10
 		}
-		num = cur
+		num = sum
 	}
 	return num
-}
-
-func main() {
-	fmt.Println(addDigits(38))
 }
