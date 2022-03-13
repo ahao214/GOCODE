6835ce6b825a78523de6344e871de19afbbe7733
ahao214 <haochen214@163.com>
2022-03-13 13:51:06 +0800

    2049. 统计最高分的节点数目

---
+++ src/ahao.leetcode/Medium/demo2049/main.go
@@ -0,0 +1,41 @@
+package main
+
+/*
+2049. 统计最高分的节点数目
+给你一棵根节点为 0 的 二叉树 ，它总共有 n 个节点，节点编号为 0 到 n - 1 。同时给你一个下标从 0 开始的整数数组 parents 表示这棵树，其中 parents[i] 是节点 i 的父节点。由于节点 0 是根，所以 parents[0] == -1 。
+
+一个子树的 大小 为这个子树内节点的数目。每个节点都有一个与之关联的 分数 。求出某个节点分数的方法是，将这个节点和与它相连的边全部 删除 ，剩余部分是若干个 非空 子树，这个节点的 分数 为所有这些子树 大小的乘积 。
+
+请你返回有 最高得分 节点的 数目 。
+*/
+func countHighestScoreNodes(parents []int) (ans int) {
+	n := len(parents)
+	children := make([][]int, n)
+	for node := 1; node < n; node++ {
+		p := parents[node]
+		children[p] = append(children[p], node)
+	}
+
+	maxScore := 0
+	var dfs func(int) int
+	dfs = func(node int) int {
+		score, size := 1, n-1
+		for _, ch := range children[node] {
+			sz := dfs(ch)
+			score *= sz
+			size -= sz
+		}
+		if node > 0 {
+			score *= size
+		}
+		if score == maxScore {
+			ans++
+		} else if score > maxScore {
+			maxScore = score
+			ans = 1
+		}
+		return n - size
+	}
+	dfs(0)
+	return
+}
