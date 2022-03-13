2da9de80bdcf39f95aed884a6022d0e136b3b3c8
ahao214 <haochen214@163.com>
2022-03-13 13:49:36 +0800

    589. N 叉树的前序遍历

---
+++ src/ahao.leetcode/Easy/demo589/main.go
@@ -0,0 +1,28 @@
+package main
+
+/*
+589. N 叉树的前序遍历
+给定一个 n 叉树的根节点  root ，返回 其节点值的 前序遍历 。
+
+n 叉树 在输入中按层序遍历进行序列化表示
+每组子节点由空值 null 分隔（请参见示例）。
+*/
+func preorder(root *Node) (ans []int) {
+	var dfs func(*Node)
+	dfs = func(node *Node) {
+		if node == nil {
+			return
+		}
+		ans = append(ans, node.Val)
+		for _, ch := range node.Children {
+			dfs(ch)
+		}
+	}
+	dfs(root)
+	return
+}
+
+type Node struct {
+	Val      int
+	Children []*Node
+}
