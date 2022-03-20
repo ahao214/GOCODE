1cc6d057a4b532dcb98ce89ff6be62a51a3c05ff
ahao214 <haochen214@163.com>
2022-03-20 16:39:25 +0800

    720. 词典中最长的单词

---
+++ src/ahao.leetcode/0700-0799/demo720/main.go
@@ -0,0 +1,70 @@
+package main
+
+import "sort"
+
+/*
+720. 词典中最长的单词
+给出一个字符串数组 words 组成的一本英语词典。返回 words 中最长的一个单词，该单词是由 words 词典中其他单词逐步添加一个字母组成。
+
+若其中有多个可行的答案，则返回答案中字典序最小的单词。若无答案，则返回空字符串。
+*/
+
+//哈希集合
+func longestWord(words []string) (longest string) {
+	sort.Slice(words, func(i, j int) bool {
+		s, t := words[i], words[j]
+		return len(s) < len(t) || len(s) == len(t) && s > t
+	})
+
+	candidates := map[string]struct{}{"": {}}
+	for _, word := range words {
+		if _, ok := candidates[word[:len(word)-1]]; ok {
+			longest = word
+			candidates[word] = struct{}{}
+		}
+	}
+	return longest
+}
+
+//字典树
+type Trie struct {
+	children [26]*Trie
+	isEnd    bool
+}
+
+func (t *Trie) Insert(word string) {
+	node := t
+	for _, ch := range word {
+		ch -= 'a'
+		if node.children[ch] == nil {
+			node.children[ch] = &Trie{}
+		}
+		node = node.children[ch]
+	}
+	node.isEnd = true
+}
+
+func (t *Trie) Search(word string) bool {
+	node := t
+	for _, ch := range word {
+		ch -= 'a'
+		if node.children[ch] == nil || !node.children[ch].isEnd {
+			return false
+		}
+		node = node.children[ch]
+	}
+	return true
+}
+
+func longestWord2(words []string) (longest string) {
+	t := &Trie{}
+	for _, word := range words {
+		t.Insert(word)
+	}
+	for _, word := range words {
+		if t.Search(word) && (len(word) > len(longest) || len(word) == len(longest) && word < longest) {
+			longest = word
+		}
+	}
+	return longest
+}
