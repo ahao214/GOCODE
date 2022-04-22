5440f7a727a095c38ec883be3a998dc0f8783273
liupenghao@fulan.com.cn <haochen214@163.com>
2022-04-22 21:25:10 +0800

    386. 字典序排数

---
+++ src/ahao.leetcode/0300-0399/demo386/main.go
@@ -0,0 +1,26 @@
+package main
+
+/*
+386. 字典序排数
+给你一个整数 n ，按字典序返回范围 [1, n] 内所有整数。
+
+你必须设计一个时间复杂度为 O(n) 且使用 O(1) 额外空间的算法。
+*/
+
+//深度优先
+func lexicalOrder(n int) []int {
+	ans := make([]int, n)
+	num := 1
+	for i := range ans {
+		ans[i] = num
+		if num*10 <= n {
+			num *= 10
+		} else {
+			for num%10 == 9 || num+1 > n {
+				num /= 10
+			}
+			num++
+		}
+	}
+	return ans
+}
