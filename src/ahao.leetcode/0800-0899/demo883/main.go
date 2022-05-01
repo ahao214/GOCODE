dfd651122b7cf98776647e505499d65341a5fb48
liupenghao@fulan.com.cn <haochen214@163.com>
2022-05-01 12:13:37 +0800

    883. 三维形体投影面积

---
+++ src/ahao.leetcode/0800-0899/demo883/main.go
@@ -0,0 +1,28 @@
+package main
+
+/*
+883. 三维形体投影面积
+*/
+func projectionArea(grid [][]int) int {
+	var xyArea, yzArea, zxArea int
+	for i, row := range grid {
+		yzHeight, zxHeight := 0, 0
+		for j, v := range row {
+			if v > 0 {
+				xyArea++
+			}
+			yzHeight = max(yzHeight, grid[j][i])
+			zxHeight = max(zxHeight, v)
+		}
+		yzArea += yzHeight
+		zxArea += zxHeight
+	}
+	return xyArea + yzArea + zxArea
+}
+
+func max(a, b int) int {
+	if b > a {
+		return b
+	}
+	return a
+}
