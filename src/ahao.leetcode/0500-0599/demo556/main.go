93e2624634fe455c89ea5b408b80cd73841997a6
liupenghao@fulan.com.cn <haochen214@163.com>
2022-07-21 13:23:21 +0800

    556. 下一个更大元素 III

--- src/ahao.leetcode/0500-0599/demo556/main.go
+++ src/ahao.leetcode/0500-0599/demo556/main.go
@@ -36,3 +36,34 @@ func reverse(a []byte) {
 
 
 //方法二：数学
+func nextGreaterElement(n int) int {
+	x, cnt := n, 1
+	for ; x >= 10 && x/10%10 >= x%10; x /= 10 {
+		cnt++
+	}
+	x /= 10
+	if x == 0 {
+		return -1
+	}
+
+	targetDigit := x % 10
+	x2, cnt2 := n, 0
+	for ; x2%10 <= targetDigit; x2 /= 10 {
+		cnt2++
+	}
+	x += x2%10 - targetDigit // 把 x2%10 换到 targetDigit 上
+
+	for i := 0; i < cnt; i++ { // 反转 n 末尾的 cnt 个数字拼到 x 后
+		d := targetDigit
+		if i != cnt2 {
+			d = n % 10
+		}
+		if x > math.MaxInt32/10 || x == math.MaxInt32/10 && d > 7 {
+			return -1
+		}
+		x = x*10 + d
+		n /= 10
+	}
+	return x
+}
+
