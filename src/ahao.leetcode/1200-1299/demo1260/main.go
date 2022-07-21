8d4c01a549cfefb2345f88f3041ffa5a860bab53
liupenghao@fulan.com.cn <haochen214@163.com>
2022-07-21 11:19:21 +0800

    1260. 二维网格迁移

---
+++ src/ahao.leetcode/1200-1299/demo1260/main.go
@@ -0,0 +1,28 @@
+package main
+
+/*
+1260. 二维网格迁移
+给你一个 m 行 n 列的二维网格 grid 和一个整数 k。你需要将 grid 迁移 k 次。
+
+每次「迁移」操作将会引发下述活动：
+
+位于 grid[i][j] 的元素将会移动到 grid[i][j + 1]。
+位于 grid[i][n - 1] 的元素将会移动到 grid[i + 1][0]。
+位于 grid[m - 1][n - 1] 的元素将会移动到 grid[0][0]。
+请你返回 k 次迁移操作后最终得到的 二维网格。
+*/
+
+func shiftGrid(grid [][]int, k int) [][]int {
+	m, n := len(grid), len(grid[0])
+	ans := make([][]int, m)
+	for i := range ans {
+		ans[i] = make([]int, n)
+	}
+	for i, row := range grid {
+		for j, v := range row {
+			index1 := (i*n + j + k) % (m * n)
+			ans[index1/n][index1%n] = v
+		}
+	}
+	return ans
+}
