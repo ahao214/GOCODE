23f747a7d830826b18524a815dca3ba7e592eea8
liupenghao@fulan.com.cn <haochen214@163.com>
2022-07-22 09:44:27 +0800

    648. 单词替换

---
+++ src/ahao.leetcode/0600-0699/demo648/main.go
@@ -0,0 +1,56 @@
+package main
+
+/*
+648. 单词替换
+*/
+
+//方法一：哈希集合
+func replaceWords(dictionary []string, sentence string) string {
+	dictionarySet := map[string]bool{}
+	for _, s := range dictionary {
+		dictionarySet[s] = true
+	}
+	words := strings.Split(sentence, " ")
+	for i, word := range words {
+		for j := 1; j <= len(word); j++ {
+			if dictionarySet[word[:j]] {
+				words[i] = word[:j]
+				break
+			}
+		}
+	}
+	return strings.Join(words, " ")
+}
+
+
+//方法二：字典树
+func replaceWords2(dictionary []string, sentence string) string {
+	type trie map[rune]trie
+	root := trie{}
+	for _, s := range dictionary {
+		cur := root
+		for _, c := range s {
+			if cur[c] == nil {
+				cur[c] = trie{}
+			}
+			cur = cur[c]
+		}
+		cur['#'] = trie{}
+	}
+
+	words := strings.Split(sentence, " ")
+	for i, word := range words {
+		cur := root
+		for j, c := range word {
+			if cur['#'] != nil {
+				words[i] = word[:j]
+				break
+			}
+			if cur[c] == nil {
+				break
+			}
+			cur = cur[c]
+		}
+	}
+	return strings.Join(words, " ")
+}
