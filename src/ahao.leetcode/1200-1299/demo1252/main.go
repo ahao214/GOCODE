d003c3ed34a9d36ff92aac745c470a487db27fdc
liupenghao@fulan.com.cn <haochen214@163.com>
2022-07-24 11:39:38 +0800

    1252. 奇数值单元格的数目

---
+++ src/ahao.leetcode/1200-1299/demo1252/main.go
@@ -0,0 +1,26 @@
+package main
+
+/*
+1252. 奇数值单元格的数目
+*/
+
+func oddCells(m, n int, indices [][]int) (ans int) {
+	matrix := make([][]int, m)
+	for i := range matrix {
+		matrix[i] = make([]int, n)
+	}
+	for _, p := range indices {
+		for j := range matrix[p[0]] {
+			matrix[p[0]][j]++
+		}
+		for _, row := range matrix {
+			row[p[1]]++
+		}
+	}
+	for _, row := range matrix {
+		for _, v := range row {
+			ans += v % 2
+		}
+	}
+	return
+}
