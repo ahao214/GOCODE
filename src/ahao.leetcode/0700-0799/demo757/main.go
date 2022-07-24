761375ffb4952233cf1b8e320257ac281a68dadf
liupenghao@fulan.com.cn <haochen214@163.com>
2022-07-24 11:46:51 +0800

    757. 设置交集大小至少为2

---
+++ src/ahao.leetcode/0700-0799/demo757/main.go
@@ -0,0 +1,27 @@
+package main
+
+/*
+757. 设置交集大小至少为2
+*/
+
+//贪心
+func intersectionSizeTwo(intervals [][]int) (ans int) {
+	sort.Slice(intervals, func(i, j int) bool {
+		a, b := intervals[i], intervals[j]
+		return a[0] < b[0] || a[0] == b[0] && a[1] > b[1]
+	})
+	n, m := len(intervals), 2
+	vals := make([][]int, n)
+	for i := n - 1; i >= 0; i-- {
+		for j, k := intervals[i][0], len(vals[i]); k < m; k++ {
+			ans++
+			for p := i - 1; p >= 0 && intervals[p][1] >= j; p-- {
+				vals[p] = append(vals[p], j)
+			}
+			j++
+		}
+	}
+	return
+}
+
+
