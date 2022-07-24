ca8c1679e875e7504db61a4a62c83fddc83f5077
liupenghao@fulan.com.cn <haochen214@163.com>
2022-07-24 11:48:22 +0800

    1184. 公交站间的距离

---
+++ src/ahao.leetcode/1100-1199/demo1184/main.go
@@ -0,0 +1,28 @@
+package main
+
+/*
+1184. 公交站间的距离
+*/
+
+func distanceBetweenBusStops(distance []int, start, destination int) int {
+	if start > destination {
+		start, destination = destination, start
+	}
+	sum1, sum2 := 0, 0
+	for i, d := range distance {
+		if start <= i && i < destination {
+			sum1 += d
+		} else {
+			sum2 += d
+		}
+	}
+	return min(sum1, sum2)
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
