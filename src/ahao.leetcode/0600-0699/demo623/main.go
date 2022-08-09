77dbaaa9421fd72b7006ef6f68dbec99b658bc08
liupenghao@fulan.com.cn <haochen214@163.com>
2022-08-09 10:30:51 +0800

    623. 在二叉树中增加一行

---
+++ src/ahao.leetcode/0600-0699/demo623/main.go
@@ -0,0 +1,30 @@
+package main
+
+/*
+623. 在二叉树中增加一行
+*/
+
+
+type TreeNode struct {
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
+
+
+func addOneRow(root *TreeNode, val, depth int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if depth == 1 {
+		return &TreeNode{val, root, nil}
+	}
+	if depth == 2 {
+		root.Left = &TreeNode{val, root.Left, nil}
+		root.Right = &TreeNode{val, nil, root.Right}
+	} else {
+		root.Left = addOneRow(root.Left, val, depth-1)
+		root.Right = addOneRow(root.Right, val, depth-1)
+	}
+	return root
+}
